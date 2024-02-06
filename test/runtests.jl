3c06d369eadd532976a268962b2fb715f5e7f99e
Samuel Brand <48288458+SamuelBrand1@users.noreply.github.com>
2024-02-06 22:58:34 +0000

    commit random walk latent process with unit test against known theoretical distribution

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,8 @@ using Test
 using RtWithoutRenewal
 using Distributions
 using LinearAlgebra
+using Turing, DynamicPPL
 
 include("test_utilities.jl")
-include("test_models.jl")
+include("test_epimodel.jl")
+include("test_latent-processes.jl")
