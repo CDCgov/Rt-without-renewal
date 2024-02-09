298eb33ed75f5e4346ceb1e8a67fd3bd958b5596
Samuel Brand <sam055@mac.com>
2024-02-09 11:48:15 +0000

    Added unit test for quadrature method against exact double censoring example

--- EpiAware/test/test_utilities.jl
+++ EpiAware/test/test_utilities.jl
@@ -45,12 +45,25 @@ end
         @test_throws AssertionError create_discrete_pmf(dist, Δd = 3.0, D = 1.0)
     end
 
-    # Test case 4: Testing output against expected PMF basic version
+    # Test case 4: Testing output against expected PMF basic version - single
+    # interval censoring
     @testset "Test case 4" begin
         dist = Exponential(1.0)
         expected_pmf = [(exp(-(t - 1)) - exp(-t)) / (1 - exp(-5)) for t = 1:5]
         pmf = create_discrete_pmf(dist, Val(:basic); Δd = 1.0, D = 5.0)
         @test pmf ≈ expected_pmf atol = 1e-15
+    end
+
+    # Test case 5: Testing output against expected PMF basic version - double
+    # interval censoring
+    @testset "Test case 5" begin
+        dist = Exponential(1.0)
+        expected_pmf_uncond = [exp(-1);
+                        [(1 - exp(-1)) * (exp(1) - 1) * exp(-s) for s = 1:9]
+                        ]
+        expected_pmf = expected_pmf_uncond ./ sum(expected_pmf_uncond)                       
+        pmf = create_discrete_pmf(dist; Δd = 1.0, D = 10.0)
+        @test expected_pmf ≈ pmf atol = 1e-15
     end
 
 end
