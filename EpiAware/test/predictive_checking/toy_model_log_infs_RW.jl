bd386d2a2b9309e7f84d9316ae60861d96f80bbd
Samuel Brand <sam055@mac.com>
2024-02-14 12:09:28 +0000

    Script for demonstrating the proposed API

--- EpiAware/test/predictive_checking/toy_model_log_infs_RW.jl
+++ EpiAware/test/predictive_checking/toy_model_log_infs_RW.jl
@@ -1,4 +1,3 @@
-using Pkg: generate
 #=
 # Toy model for running analysis:
 
@@ -47,18 +46,16 @@ r &\sim \text{Gamma}(3, 0.05/3).
 \end{align}
 ```
 
-## Load dependencies in `TestEnv`
+## Load dependencies
+
+This script should be run from the root folder of `EpiAware` and with the active environment.
 
 =#
 
-split(pwd(), "/")[end] != "EpiAware" && begin
-    cd("./EpiAware")
-    using Pkg
-    Pkg.activate(".")
 
-    using TestEnv
-    TestEnv.activate()
-end
+
+using TestEnv # Run in Test environment mode
+TestEnv.activate()
 
 using EpiAware
 using Turing
@@ -70,16 +67,18 @@ Random.seed!(0)
 
 #=
 ## Create an `EpiModel` struct
-Somewhat randomly chosen parameters for the `EpiModel` struct.
 
+- Medium length generation interval distribution.
+- Median 2 day, std 4.3 day delay distribution.
+- 100 days of simulations
 =#
 
-truth_GI = Gamma(1, 2)
-truth_delay = Uniform(0.0, 5.0)
+truth_GI = Gamma(2, 5)
+truth_delay = LogNormal(2.0, 1.0)
 neg_bin_cluster_factor = 0.05
 time_horizon = 100
 
-epimodel = EpiModel(
+model_data = EpiData(
     truth_GI,
     truth_delay,
     neg_bin_cluster_factor,
@@ -89,29 +88,41 @@ epimodel = EpiModel(
 )
 
 #=
-## Define a log-infections model
-The log-infections model is defined by a Turing model `log_infections`.
+## Define the data generating process
 
-In this case we don't have observed data, so we use `missing` value for `y_t`.
+In this case we use the `DirectInfections` model.
 =#
-toy_log_infs = log_infections(
+
+toy_log_infs = DirectInfections(model_data)
+
+#=
+## Generate a `Turing` `Model`
+We don't have observed data, so we use `missing` value for `y_t`.
+=#
+
+log_infs_model = make_epi_inference_model(
     missing,
-    epimodel,
-    random_walk;
-    latent_process_priors = EpiAware.STANDARD_RW_PRIORS,
+    toy_log_infs,
+    random_walk,
+    latent_process_priors = default_rw_priors(),
+    pos_shift = 1e-6,
+    neg_bin_cluster_factor = 0.5,
+    neg_bin_cluster_factor_prior = Gamma(3, 0.05 / 3),
 )
+
+
 
 #=
 ## Sample from the model
 I define a fixed version of the model with initial infections set to 10 and variance of the random walk process set to 0.1.
 We can sample from the model using the `rand` function, and plot the generated infections against generated cases.
 =#
-cond_toy = fix(toy_log_infs, (init_rw_value = log(10.0), σ²_RW = 0.1))
-random_epidemic = rand(cond_toy)
-
 # We can get the generated infections using `generated_quantities` function. Because the observed
 # cases are "defined" with a `~` operator they can be accessed directly from the randomly sampled
 # process.
+
+cond_toy = fix(log_infs_model, (init_rw_value = log(10.0), σ²_RW = 0.1))
+random_epidemic = rand(cond_toy)
 gen = generated_quantities(cond_toy, random_epidemic)
 plot(
     gen.I_t,
@@ -120,4 +131,4 @@ plot(
     ylabel = "Infections",
     title = "Generated Infections",
 )
-scatter!(X.y_t, lab = "generated cases")
+scatter!(random_epidemic.y_t, lab = "generated cases")
