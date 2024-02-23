b72e0a3722aa24b7712dfc608db19eabe5f0d275
Sam <s.e.abbott12@gmail.com>
2024-02-23 19:20:59 +0000

    partial documenter setup

--- EpiAware/docs/make.jl
+++ EpiAware/docs/make.jl
@@ -3,8 +3,16 @@ using EpiAware
 
 include("pages.jl")
 
+# Generate a Documenter-friendly changelog from CHANGELOG.md
+Changelog.generate(
+    Changelog.Documenter(),
+    joinpath(@__DIR__, "..", "CHANGELOG.md"),
+    joinpath(@__DIR__, "src", "release-notes.md");
+    repo = "JuliaDocs/Documenter.jl"
+)
+
 makedocs(; sitename = "EpiAware.jl",
-    authors = "Samuel Brand, Zachary Susswein, and Sam Abbott",
+    authors = "Samuel Brand, Zachary Susswein, Sam Abbott, and contributors",
     clean = true, doctest = true, linkcheck = true,
     warnonly = [:docs_block, :missing_docs],
     modules = [EpiAware],
