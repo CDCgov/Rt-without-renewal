a0e91feebd41d2c89bd43ef55d1831dd2fb8489f
Samuel Brand <sam055@mac.com>
2024-02-27 12:26:18 +0000

    remove undefined exports and fix broken unit test due to default priors not being exported (used internally now)

--- EpiAware/src/EpiAware.jl
+++ EpiAware/src/EpiAware.jl
@@ -32,16 +32,13 @@ using Distributions,
       DataFramesMeta
 
 # Exported utilities
-export create_discrete_pmf,
-       default_rw_priors, default_delay_obs_priors,
-       default_initialisation_prior, spread_draws
+export create_discrete_pmf, spread_draws
 
 # Exported types
 export EpiData, Renewal, ExpGrowthRate, DirectInfections
 
 # Exported Turing model constructors
-export make_epi_inference_model, delay_observations_model,
-       initialize_incidence
+export make_epi_inference_model
 
 include("epimodel.jl")
 include("utilities.jl")
