3d704cf8462c918dd92348ee04a2f08865029cf4
Samuel Brand <sam055@mac.com>
2024-02-29 13:23:13 +0000

    More side page links

--- EpiAware/docs/pages.jl
+++ EpiAware/docs/pages.jl
@@ -1,3 +1,8 @@
 pages = [
-    "EpiAware.jl: Real-time epidemic monitoring" => "index.md"
+    "EpiAware.jl: Real-time epidemic monitoring" => "index.md",
+    "Examples" => [
+        "Double censoring" => "man/discretized_pmfs.md",
+        "Calculating exponential growth rate from reproduction number" => "man/R_to_r.md",
+        "Running inference for a toy model" => "man/toy_model_log_infs_RW.md"
+    ]
 ]
