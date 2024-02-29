4ea3c1294e578bcd1dfd8d77e6adf7bee0aa3258
Sam <s.e.abbott12@gmail.com>
2024-02-29 11:45:17 +0000

    add additional docs structure"

---
+++ EpiAware/docs/changelog.jl
@@ -0,0 +1,7 @@
+# Generate a Documenter-friendly changelog from CHANGELOG.md
+Changelog.generate(
+    Changelog.Documenter(),
+    joinpath(@__DIR__, "..", "CHANGELOG.md"),
+    joinpath(@__DIR__, "src", "release-notes.md");
+    repo = "JuliaDocs/Documenter.jl"
+)
