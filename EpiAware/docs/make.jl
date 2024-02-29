1dd1fbf1c5131c8b98bb3305eeebf36d0f368d04
Samuel Brand <sam055@mac.com>
2024-02-29 13:22:56 +0000

    commented out `linkcheck` and set HTML format to detect `CI` mode

--- EpiAware/docs/make.jl
+++ EpiAware/docs/make.jl
@@ -1,12 +1,23 @@
-using Documenter
+using Pkg
+Pkg.instantiate() #This expecting to be run within the EpiAware/docs environment
+push!(LOAD_PATH,
+    joinpath(@__DIR__(), "../src/")
+)
+
+using Documenter, Changelog
 using EpiAware
+# using EpiAware
 
 include("changelog.jl")
 include("pages.jl")
 
 makedocs(; sitename = "EpiAware.jl",
     authors = "Samuel Brand, Zachary Susswein, Sam Abbott, and contributors",
-    clean = true, doctest = true, linkcheck = true,
+    clean = true, doctest = true, #linkcheck = true,
     warnonly = [:docs_block, :missing_docs],
     modules = [EpiAware],
-    pages = pages)
+    pages = pages,
+    format = Documenter.HTML(
+        prettyurls = get(ENV, "CI", nothing) == "true"
+    )
+)
