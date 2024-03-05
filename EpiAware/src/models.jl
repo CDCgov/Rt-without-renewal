2132d23c25ef4c6204997a6db6bc77ec3c7a52cc
Samuel Brand <sam055@mac.com>
2024-03-05 11:22:25 +0000

    add a safe mode for `make_epi_aware`

--- EpiAware/src/models.jl
+++ EpiAware/src/models.jl
@@ -25,3 +25,38 @@
         latent_model,
         process_aux = merge(latent_model_aux, generated_y_t_aux))
 end
+
+@model function make_epi_aware(y_t,
+        time_steps,
+        ::Val{:safe};
+        epi_model::AbstractEpiModel,
+        latent_model_model::AbstractLatentModel,
+        observation_model::AbstractObservationModel,
+        pos_shift = 1e-6)
+    try
+        #Latent process
+        @submodel latent_model, latent_model_aux = generate_latent(
+            latent_model_model,
+            time_steps)
+
+        #Transform into infections
+        @submodel I_t = generate_latent_infs(epi_model, latent_model)
+
+        #Predictive distribution of ascerted cases
+        @submodel generated_y_t, generated_y_t_aux = generate_observations(
+            observation_model,
+            y_t,
+            I_t;
+            pos_shift = pos_shift)
+
+        #Generate quantities
+        return (;
+            generated_y_t,
+            I_t,
+            latent_model,
+            process_aux = merge(latent_model_aux, generated_y_t_aux))
+    catch
+        Turing.@addlogprob! -Inf
+        return
+    end
+end
