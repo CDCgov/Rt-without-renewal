fc349d7d68a9262427780bf9ca171d5838fa2162
Samuel Brand <48288458+SamuelBrand1@users.noreply.github.com>
2024-03-06 13:33:38 +0000

    Add a getting started tutorial (#109)
    * update
    
    * added doc deps
    
    * started `getting started` notebook
    
    * Update getting_started.jl
    
    * Getting started example
    
    * Add generated Pluto notebooks to gitignore
    
    * remove old version of example
    
    * add Pluto and PlutoStaticHTML as deps
    
    * Adapt make and build files to generate Pluto notebooks statically and then render into docs
    
    * include build step for rendering Pluto notebooks
    
    * remove old version of the getting started example
    
    * Update EpiAware/docs/src/examples/getting_started.jl
    
    ---------
    
    Co-authored-by: Sam Abbott <azw1@cdc.gov>

--- EpiAware/docs/make.jl
+++ EpiAware/docs/make.jl
@@ -1,8 +1,13 @@
 using Documenter
 using EpiAware
+using Pluto: Configuration.CompilerOptions
+using PlutoStaticHTML
 
 include("changelog.jl")
 include("pages.jl")
+include("build.jl")
+
+build("examples")
 
 makedocs(; sitename = "EpiAware.jl",
     authors = "Samuel Brand, Zachary Susswein, Sam Abbott, and contributors",
@@ -11,7 +16,10 @@ makedocs(; sitename = "EpiAware.jl",
     modules = [EpiAware],
     pages = pages,
     format = Documenter.HTML(
-        prettyurls = get(ENV, "CI", nothing) == "true"
+        prettyurls = get(ENV, "CI", nothing) == "true",
+        mathengine = Documenter.MathJax3(),
+        size_threshold = 600 * 2^10,
+        size_threshold_warn = 200 * 2^10
     )
 )
 
