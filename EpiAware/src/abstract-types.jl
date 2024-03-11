fe20c7c07f36275bee765c59e958163cfc4c2fb1
Samuel Brand <sam055@mac.com>
2024-03-11 22:58:49 +0000

    Start documenting generate_latent_infs

--- EpiAware/src/abstract-types.jl
+++ EpiAware/src/abstract-types.jl
@@ -6,7 +6,20 @@ abstract type AbstractLatentModel <: AbstractModel end
 
 abstract type AbstractObservationModel <: AbstractModel end
 
-function generate_latent_infs(epi_model::AbstractEpiModel, latent_model)
+@doc raw"""
+Generate latent infections based on the given epidemiological model and a latent process
+path ``Z_t``.
+
+# Arguments
+- `epi_model::DirectInfections`: The epidemiological model used to generate latent infections.
+- `Z_t`: The noise term.
+
+# Returns
+- An array of generated latent infections.
+
+# Example
+"""
+function generate_latent_infs(epi_model::AbstractEpiModel, Z_t)
     @info "No concrete implementation for `generate_latent_infs` is defined."
     return nothing
 end
