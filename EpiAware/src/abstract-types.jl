0863b2965543abd696e50bf714fd29394739acc5
Samuel Brand <48288458+SamuelBrand1@users.noreply.github.com>
2024-03-12 23:14:11 +0000

    doc string generate_latent function

--- EpiAware/src/abstract-types.jl
+++ EpiAware/src/abstract-types.jl
@@ -38,6 +38,22 @@ function generate_latent_infs(epi_model::AbstractEpiModel, Z_t)
     return nothing
 end
 
+@doc raw"""
+Constructor function for a latent process path ``Z_t`` of length `n`.
+
+The `generate_latent` function implements a model of generating a latent process. Which
+model for generating the latent process infections is implemented is set by the type of
+`latent_model`. If no implemention is defined for the type of `latent_model`, then
+`EpiAware` will pass a warning and return `nothing`.
+
+## Interface to `Turing.jl` probablilistic programming language (PPL)
+
+Apart from the no implementation fallback method, the `generate_latent` implementation
+function should return a constructor function for a
+    [`DynamicPPL.Model`](https://turinglang.org/DynamicPPL.jl/stable/api/#DynamicPPL.Model)
+object. Sample paths of ``Z_t`` are generated quantities of the constructed model. Priors
+for model parameters are fields of `epi_model`.
+"""
 function generate_latent(latent_model::AbstractLatentModel, n)
     @info "No concrete implementation for generate_latent is defined."
     return nothing
