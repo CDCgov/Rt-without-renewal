067fb4b864ff86cb0a57082d93c7001d140bcda6
Samuel Brand <48288458+SamuelBrand1@users.noreply.github.com>
2024-03-12 23:15:43 +0000

    doc string RandomWalk

--- EpiAware/src/latentmodels/randomwalk.jl
+++ EpiAware/src/latentmodels/randomwalk.jl
@@ -1,3 +1,55 @@
+@doc raw"
+Model latent process ``Z_t`` as a random walk.
+
+## Mathematical specification
+
+The random walk ``Z_t`` is specified as a parameteric transformation of the white noise
+sequence ``(\epsilon_t)_{t\geq 1}``,
+
+```math
+Z_t = Z_0 + \sigma \sum_{i = 1}^t \epsilon_t
+```
+
+Constructing a random walk requires specifying:
+- An `init_prior` as a prior for ``Z_0``. Default is `Normal()`.
+- A `std_prior` for ``\sigma``. The default is HalfNormal with a mean of 0.25.
+
+## Constructors
+
+- `RandomWalk(; init_prior, std_prior)`
+
+## Example usage with `generate_latent`
+
+`generate_latent` can be used to construct a `Turing` model for the random walk ``Z_t``.
+
+First, we construct a `RandomWalk` struct with priors,
+
+```julia
+using Distributions, Turing, EpiAware
+
+# Create a RandomWalk model
+rw = RandomWalk(init_prior = Normal(2., 1.),
+                                std_prior = _make_halfnormal_prior(0.1))
+```
+
+Then, we can use `generate_latent` to construct a Turing model for a 10 step random walk.
+
+```julia
+# Construct a Turing model
+rw_model = generate_latent(rw, 10)
+```
+
+Now we can use the `Turing` PPL API to sample underlying parameters and generate the
+unobserved infections.
+
+```julia
+#Sample random parameters from prior
+θ = rand(rw_model)
+#Get random walk sample path as a generated quantities from the model
+Z_t, _ = generated_quantities(rw_model, θ)
+```
+
+"
 @kwdef struct RandomWalk{D <: Sampleable, S <: Sampleable} <: AbstractLatentModel
     init_prior::D
     std_prior::S
