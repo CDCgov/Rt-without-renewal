200cb1a484b5cec182524ae6eba2570b1c747db4
Samuel Brand <sam055@mac.com>
2024-03-12 16:13:45 +0000

    Add and make consistent doc strings and example usage for EpiModels and EpiData

--- EpiAware/src/abstract-types.jl
+++ EpiAware/src/abstract-types.jl
@@ -1,5 +1,11 @@
 abstract type AbstractModel end
 
+"""
+    abstract type AbstractEpiModel <: AbstractModel end
+
+The abstract supertype for all structs that define a model for generating unobserved/latent
+    infections.
+"""
 abstract type AbstractEpiModel <: AbstractModel end
 
 abstract type AbstractLatentModel <: AbstractModel end
@@ -7,20 +13,24 @@ abstract type AbstractLatentModel <: AbstractModel end
 abstract type AbstractObservationModel <: AbstractModel end
 
 @doc raw"""
-Generate latent infections based on the given epidemiological model and a latent process
-path ``Z_t``.
+Generate unobserved/latent infections based on the given `epi_model <: AbstractEpimodel`
+    and a latent process path ``Z_t``.
 
-# Arguments
-- `epi_model::DirectInfections`: The epidemiological model used to generate latent infections.
-- `Z_t`: The noise term.
+The `generate_latent_infs` function implements a model of generating unobserved/latent
+infections conditional on a latent process. Which model of generating unobserved/latent
+infections to be implemented is set by the type of `epi_model`. If no implemention is
+defined for the given `epi_model`, then `EpiAware` will return a warning and return
+`nothing`.
 
-# Returns
-- An array of generated latent infections.
+## Interface to `Turing.jl` probablilistic programming language (PPL)
 
-# Example
+Apart from the no implementation fallback method, the `generate_latent_infs` implementation
+function should be a constructor function for a
+    [`DynamicPPL.Model`](https://turinglang.org/DynamicPPL.jl/stable/api/#DynamicPPL.Model)
+    object.
 """
 function generate_latent_infs(epi_model::AbstractEpiModel, Z_t)
-    @info "No concrete implementation for `generate_latent_infs` is defined."
+    @warn "No concrete implementation for `generate_latent_infs` is defined."
     return nothing
 end
 
--- EpiAware/src/epimodels/directinfections.jl
+++ EpiAware/src/epimodels/directinfections.jl
@@ -1,15 +1,19 @@
 @doc raw"
-Defines the model for latent infections as a transformation on a sampled latent model.
+Model unobserved/latent infections as a transformation on a sampled latent process.
 
 ## Mathematical specification
 
-If ``Z_t`` is a realisation of the latent model, then the latent infections are given by
+If ``Z_t`` is a realisation of the latent model, then the unobserved/latent infections are
+given by
 
 ```math
-I_t = g(Z_t).
+I_t = g(\hat{I}_0 + Z_t).
 ```
 
-where ``g`` is a transformation function.
+where ``g`` is a transformation function and the unconstrained initial infections
+    ``\hat{I}_0`` are sampled from a prior distribution, `initialisation_prior` which must
+be supplied to the `DirectInfections` constructor. The default `initialisation_prior` is
+the standard Normal `Distributions.Normal()`.
 
 ## Constructor
 
@@ -18,24 +22,46 @@ where ``g`` is a transformation function.
 
 ## Example usage with `generate_latent_infs`
 
-Create a Turing model for the latent infections conditional on the sample path of a white
-    noise latent process.
+`generate_latent_infs` can be used to construct a `Turing` model for the latent infections
+conditional on the sample path of a latent process. In this example, we generate a sample
+of a white noise latent process.
+
+First, we construct a `DirectInfections` struct with an `EpiData` object, an initialisation
+prior and a transformation function.
 
 ```julia
 using Distributions, Turing, EpiAware
 gen_int = [0.2, 0.3, 0.5]
-transformation = exp
+g = exp
 
 # Create an EpiData object
-data = EpiData(gen_int, transformation)
+data = EpiData(gen_int, g)
 
 # Create a DirectInfections model
 direct_inf_model = DirectInfections(data = data, initialisation_prior = Normal())
+```
 
-# Generate latent infections
+Then, we can use `generate_latent_infs` to construct a Turing model for the unobserved
+infection generation model set by the type of `direct_inf_model`.
+
+```julia
+# Construct a Turing model
 Z_t = randn(100)
 latent_inf = generate_latent_infs(direct_inf_model, Z_t)
 ```
+
+Now we can use the `Turing` PPL API to sample underlying parameters and generate the
+unobserved infections.
+
+```julia
+# Sample from the unobserved infections model
+
+#Sample random parameters from prior
+θ = rand(latent_inf)
+#Get unobserved infections as a generated quantities from the model
+I_t = generated_quantities(latent_inf, θ)
+```
+
 "
 @kwdef struct DirectInfections{S <: Sampleable} <: AbstractEpiModel
     "`Epidata` object."
@@ -45,7 +71,43 @@ latent_inf = generate_latent_infs(direct_inf_model, Z_t)
 end
 
 """
-Generate latent infections based on the given epidemiological model and noise term.
+Implement the `generate_latent_infs` function for the `DirectInfections` model.
+
+## Example usage with `DirectInfections` type of model for unobserved infection process
+
+First, we construct a `DirectInfections` struct with an `EpiData` object, an initialisation
+prior and a transformation function.
+
+```julia
+using Distributions, Turing, EpiAware
+gen_int = [0.2, 0.3, 0.5]
+g = exp
+
+# Create an EpiData object
+data = EpiData(gen_int, g)
+
+# Create a DirectInfections model
+direct_inf_model = DirectInfections(data = data, initialisation_prior = Normal())
+```
+
+Then, we can use `generate_latent_infs` to construct a Turing model for the unobserved
+infection generation model set by the type of `direct_inf_model`.
+
+```julia
+# Construct a Turing model
+Z_t = randn(100)
+latent_inf = generate_latent_infs(direct_inf_model, Z_t)
+```
+Now we can use the `Turing` PPL API to sample underlying parameters and generate the
+unobserved infections.
+```julia
+# Sample from the unobserved infections model
+
+#Sample random parameters from prior
+θ = rand(latent_inf)
+#Get unobserved infections as a generated quantities from the model
+I_t = generated_quantities(latent_inf, θ)
+```
 """
 @model function generate_latent_infs(epi_model::DirectInfections, Z_t)
     init_incidence ~ epi_model.initialisation_prior
