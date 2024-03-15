d97c414125340a96937e957008b7ad5892734b92
Samuel Brand <sam055@mac.com>
2024-03-15 15:05:09 +0000

    EpiAwareMethod structs

--- EpiAware/src/EpiInference/EpiInference.jl
+++ EpiAware/src/EpiInference/EpiInference.jl
@@ -1,16 +1,21 @@
-module EpiInference
 
 """
 Module for defining inference methods.
 """
+module EpiInference
 
+using ..EpiAwareBase: AbstractEpiAwareMethod, AbstractEpiAwareOptMethod,
+                      AbstractEpiAwareSamplingMethod
 using Pathfinder: pathfinder, PathfinderResult
 
 using DynamicPPL, DocStringExtensions
 
+export AbstractNUTSMethod
+export ManyPathfinderMethod
 export manypathfinder
 
 include("docstrings.jl")
 include("manypathfinder.jl")
+include("nuts.jl")
 
 end
