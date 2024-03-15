4cbb74d1cac6b43ef7812a3a1093348ee21c833e
Samuel Brand <sam055@mac.com>
2024-03-15 10:32:01 +0000

    Change `pos_shift` into a property of ObservationModel

--- EpiAware/src/EpiObsModels/delayobservations.jl
+++ EpiAware/src/EpiObsModels/delayobservations.jl
@@ -1,17 +1,19 @@
 struct DelayObservations{T <: AbstractFloat, S <: Sampleable} <: AbstractObservationModel
     delay_kernel::SparseMatrixCSC{T, Integer}
     neg_bin_cluster_factor_prior::S
+    pos_shift::T
 
     function DelayObservations(delay_int,
             time_horizon,
-            neg_bin_cluster_factor_prior)
+            neg_bin_cluster_factor_prior;
+            pos_shift = 1e-6)
         @assert all(delay_int .>= 0) "Delay interval must be non-negative"
         @assert sum(delay_int)≈1 "Delay interval must sum to 1"
 
         K = generate_observation_kernel(delay_int, time_horizon)
 
         new{eltype(K), typeof(neg_bin_cluster_factor_prior)}(K,
-            neg_bin_cluster_factor_prior)
+            neg_bin_cluster_factor_prior, pos_shift)
     end
 
     function DelayObservations(;
@@ -19,9 +21,11 @@ struct DelayObservations{T <: AbstractFloat, S <: Sampleable} <: AbstractObserva
             time_horizon::Integer,
             neg_bin_cluster_factor_prior::Sampleable,
             D_delay,
-            Δd = 1.0)
+            Δd = 1.0,
+            pos_shift = 1e-6)
         delay_int = create_discrete_pmf(delay_distribution; Δd = Δd, D = D_delay)
-        return DelayObservations(delay_int, time_horizon, neg_bin_cluster_factor_prior)
+        return DelayObservations(
+            delay_int, time_horizon, neg_bin_cluster_factor_prior; pos_shift)
     end
 end
 
@@ -32,14 +36,13 @@ end
 
 @model function EpiAwareBase.generate_observations(observation_model::DelayObservations,
         y_t,
-        I_t;
-        pos_shift)
+        I_t)
 
     #Parameters
     neg_bin_cluster_factor ~ observation_model.neg_bin_cluster_factor_prior
 
     #Predictive distribution
-    expected_obs = observation_model.delay_kernel * I_t .+ pos_shift
+    expected_obs = observation_model.delay_kernel * I_t .+ observation_model.pos_shift
 
     if ismissing(y_t)
         y_t = Vector{Int}(undef, length(expected_obs))
