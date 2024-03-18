1521c545c379179f5909d6036749e7f1950e5328
Samuel Brand <48288458+SamuelBrand1@users.noreply.github.com>
2024-03-18 12:59:32 +0000

    move epi_aware functions into src folder

---
+++ EpiAware/src/epiawareprob.jl
@@ -0,0 +1,22 @@
+"""
+Defines an inference/generative modelling problem for case data.
+
+`EpiProblem` wraps the underlying components of an epidemiological model:
+- `epi_model`: An epidemiological model for unobserved infections.
+- `latent_model`: A latent model for underlying latent process.
+- `observation_model`: An observation model for observed cases.
+
+Along with a `tspan` tuple for the time span of the case data.
+"""
+@kwdef struct EpiProblem{
+    E <: AbstractEpiModel, L <: AbstractLatentModel, O <: AbstractObservationModel} <:
+              AbstractEpiProblem
+    "Epidemiological model for unobserved infections."
+    epi_model::E
+    "Latent model for underlying latent process."
+    latent_model::L
+    "Observation model for observed cases."
+    observation_model::O
+    "Time span for either inference or generative modelling of case time series."
+    tspan::Tuple{Int, Int}
+end
