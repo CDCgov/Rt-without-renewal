6a6a1d4b03d6fa8759b222bcb375b2d57b58295c
Sam Abbott <azw1@cdc.gov>
2024-03-28 09:58:07 +0000

    Issue 51: Adds Ascertainment (#175)
    * initial pass at ascertainment
    
    * make a working example
    
    * initial testing
    
    * add tests
    
    * check tests

---
+++ EpiAware/test/EpiLatentModels/Ascertainment.jl
@@ -0,0 +1,39 @@
+@testitem "Test Ascertainment constructor" begin
+    using Turing
+    struct Scale <: AbstractLatentModel
+    end
+
+    @model function EpiAware.generate_latent(model::Scale, n::Int)
+        scale = 0.1
+        scale_vect = fill(scale, n)
+        return scale_vect, (; scale = scale)
+    end
+
+    function natural(x)
+        return x
+    end
+
+    asc = Ascertainment(NegativeBinomialError(), Scale(), natural)
+    @test asc.model == NegativeBinomialError()
+    @test asc.latentmodel == Scale()
+    @test asc.link == natural
+end
+
+# make a test based on above example
+@testitem "Test Ascertainment generate_observations" begin
+    using Turing, DynamicPPL
+    struct Scale <: AbstractLatentModel end
+
+    @model function EpiAware.generate_latent(model::Scale, n::Int)
+        scale = 0.1
+        scale_vect = fill(scale, n)
+        return scale_vect, (; scale = scale)
+    end
+    obs = Ascertainment(NegativeBinomialError(), Scale(), x -> x)
+    gen_obs = generate_observations(obs, missing, fill(100, 10))
+    samples = sample(gen_obs, Prior(), 100; progress = false)
+    gen = mapreduce(vcat, generated_quantities(gen_obs, samples)) do gen
+        gen[2][:expected_obs]
+    end
+    @test all(gen .== 10.0)
+end
