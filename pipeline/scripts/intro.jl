6ca0cbe58a3660f7a167d9ede99d5416bf012774
Samuel Brand <sam055@mac.com>
2024-05-01 12:08:58 +0100

    fix formatting

--- pipeline/scripts/intro.jl
+++ pipeline/scripts/intro.jl
@@ -5,15 +5,15 @@ using DrWatson
 include(srcdir("dummy_src_file.jl"))
 
 println(
-"""
-Currently active project is: $(projectname())
+    """
+    Currently active project is: $(projectname())
 
-Path of active project: $(projectdir())
+    Path of active project: $(projectdir())
 
-Have fun with your new project!
+    Have fun with your new project!
 
-You can help us improve DrWatson by opening
-issues on GitHub, submitting feature requests,
-or even opening your own Pull Requests!
-"""
+    You can help us improve DrWatson by opening
+    issues on GitHub, submitting feature requests,
+    or even opening your own Pull Requests!
+    """
 )
--- pipeline/test/runtests.jl
+++ pipeline/test/runtests.jl
@@ -14,4 +14,4 @@ end
 
 ti = time() - ti
 println("\nTest took total time of:")
-println(round(ti/60, digits = 3), " minutes")
+println(round(ti / 60, digits = 3), " minutes")
