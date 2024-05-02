9f3b88722be88cda0d7f25975c3126c13cfd53ae
Samuel Brand <sam055@mac.com>
2024-05-02 14:12:57 +0100

    Make analysis pipeline src code into a module with a config struct to dispatch truth sims on

---
+++ pipeline/src/AnalysisPipeline.jl
@@ -0,0 +1,15 @@
+"""
+This module contains the analysis pipeline for the `Rt-without-renewal` project.
+"""
+module AnalysisPipeline
+
+using CSV, Dagger, DataFramesMeta, Dates, Distributions, DocStringExtensions, DrWatson,
+      EpiAware, Plots, Statistics
+
+export TruthSimulationConfig
+export simulate_or_infer
+
+include("docstrings.jl")
+include("TruthSimulationConfig.jl")
+
+end
---
+++ pipeline/src/TruthSimulationConfig.jl
@@ -0,0 +1,60 @@
+"""
+A configuration struct for truth simulation to compare to inference.
+"""
+@kwdef struct TruthSimulationConfig{T <: Real, F <: Function}
+    "True time-varying process. Default this is the time-varing reproductive
+    number"
+    truth_process::Vector{T}
+    "True generation interval distribution."
+    generation_distribution::Distribution
+    "Infection-generating model type. Default is `:Renewal`."
+    igp::UnionAll = Renewal
+    "Maximum next generation interval when discretized. Default is 21 days."
+    D_gen::T = 21.0
+    "Transformation function"
+    transformation::F = exp
+    "Delay distribution: Default is Gamma(4, 5/4)."
+    delay_distribution::Distribution = Gamma(4, 5 / 4)
+    "Maximum delay when discretized. Default is 15 days."
+    D_obs::T = 15.0
+end
+
+"""
+    simulate_or_infer(config::TruthSimulationConfig)
+
+Simulates or infers the truth process and observations based on the given configuration.
+
+# Arguments
+- `config::TruthSimulationConfig`: The configuration object containing the parameters for the simulation.
+
+# Returns
+A dictionary containing the sampled infections and observations, along with other relevant information.
+
+"""
+function simulate_or_infer(config::TruthSimulationConfig)
+    #Define infection-generating model
+    model_data = EpiData(gen_distribution = config.generation_distribution,
+        D_gen = config.D_gen,
+        transformation = config.transformation)
+
+    log_I0_prior = Normal(log(10.0), 1e-5)
+    epi = config.igp(model_data, log_I0_prior)
+
+    # Sample infections
+    inf_mdl = generate_latent_infs(epi, log.(config.truth_process))
+    I_t = inf_mdl()
+
+    #Define the infection conditional observation distribution
+    obs = LatentDelay(NegativeBinomialError(), config.delay_distribution; D = config.D_obs)
+
+    #Sample observations
+    obs_model = generate_observations(obs, missing, I_t)
+    yt, θ = obs_model()
+
+    #Return the sampled infections and observations
+
+    return Dict("I_t" => I_t, "y_t" => yt, "cluster_factor" => θ.cluster_factor,
+        "truth_process" => config.truth_process,
+        "truth_gi_mean" => mean(config.generation_distribution),
+        "truth_gi_std" => std(config.generation_distribution))
+end
