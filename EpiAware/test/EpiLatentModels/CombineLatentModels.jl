3e0738cdc2ed93b47afb25e5595696cef7a5f368
Sam <s.e.abbott12@gmail.com>
2024-05-07 19:07:21 +0100

    add basic tests for CombineLatentModels

--- EpiAware/test/EpiLatentModels/CombineLatentModels.jl
+++ EpiAware/test/EpiLatentModels/CombineLatentModels.jl
@@ -1,1 +1,37 @@
 
+@testitem "CombineLatentModels constructor works as expected" begin
+    using Distributions: Normal
+    int = Intercept(Normal(0, 1))
+    ar = AR()
+    comb = CombineLatentModels([int, ar])
+    @test typeof(comb) <: AbstractTuringLatentModel
+    @test comb.models == [int, ar]
+end
+
+@testitem "CombineLatentModels generate_latent method works as expected" begin
+    using Turing
+    struct Scale <: AbstractTuringLatentModel end
+    @model function EpiAware.EpiAwareBase.generate_latent(model::Scale, n::Int)
+        scale = 1
+        return scale_vect = fill(scale, n), (; scale)
+    end
+
+    struct NextScale <: AbstractTuringLatentModel end
+
+    @model function EpiAware.EpiAwareBase.generate_latent(model::NextScale, n::Int)
+        scale = 2
+        return scale_vect = fill(scale, n), (; nscale = scale)
+    end
+
+    s = Scale()
+    ns = NextScale()
+    comb = CombineLatentModels([s, ns])
+    comb_model = generate_latent(comb, 5)
+    comb_model_out = comb_model()
+
+    @test typeof(comb_model) <: DynamicPPL.Model
+    @test length(comb_model_out[1]) == 5
+    @test all(comb_model_out[1] .== fill(3.0, 5))
+    @test comb_model_out[2].scale == 1.0
+    @test comb_model_out[2].nscale == 2.0
+end
