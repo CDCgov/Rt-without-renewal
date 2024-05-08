f9192904f82de0b5de711e14b01529fe7f476a97
Sam <s.e.abbott12@gmail.com>
2024-05-08 12:20:19 +0100

    fix FixedIntecept tests and drop uses of scale where we can

--- EpiAware/test/EpiObsModels/Ascertainment.jl
+++ EpiAware/test/EpiObsModels/Ascertainment.jl
@@ -1,35 +1,21 @@
 @testitem "Test Ascertainment constructor" begin
     using Turing
-    struct Scale <: AbstractTuringLatentModel
-    end
-
-    @model function EpiAware.generate_latent(model::Scale, n::Int)
-        scale = 0.1
-        scale_vect = fill(scale, n)
-        return scale_vect, (; scale = scale)
-    end
-
     function natural(x)
         return x
     end
 
-    asc = Ascertainment(NegativeBinomialError(), Scale(), natural)
+    asc = Ascertainment(NegativeBinomialError(), FixedIntercept(0.1), natural)
     @test asc.model == NegativeBinomialError()
-    @test asc.latentmodel == Scale()
+    @test asc.latentmodel == FixedIntercept(0.1)
     @test asc.link == natural
 end
 
 # make a test based on above example
 @testitem "Test Ascertainment generate_observations" begin
     using Turing, DynamicPPL
-    struct Scale <: AbstractTuringLatentModel end
 
-    @model function EpiAware.generate_latent(model::Scale, n::Int)
-        scale = 0.1
-        scale_vect = fill(scale, n)
-        return scale_vect, (; scale = scale)
     end
-    obs = Ascertainment(NegativeBinomialError(), Scale(), x -> x)
+    obs = Ascertainment(NegativeBinomialError(), FixedIntercept(0.1), x -> x)
     gen_obs = generate_observations(obs, missing, fill(100, 10))
     samples = sample(gen_obs, Prior(), 100; progress = false)
     gen = mapreduce(vcat, generated_quantities(gen_obs, samples)) do gen
