cd196efed178ee6ffa27e5a7fabd4bac82d0843b
Samuel Brand <sam055@mac.com>
2024-05-08 17:11:23 +0100

    Add common parameters description

--- pipeline/scripts/common_param_values.jl
+++ pipeline/scripts/common_param_values.jl
@@ -1,3 +1,11 @@
+@info("""
+      Setting common parameter in `Main` module.
+      ---------------------------------------------
+      Currently active project is: $(projectname())
+      Path of active project: $(projectdir())
+
+      """)
+
 # Common generation interval values
 gi_means = [2.0, 10.0, 20.0];
 gi_stds = [2.0];
