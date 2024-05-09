b81fe22dd7e66c6c9f74ad58bf0308bef7923dd0
Samuel Brand <sam055@mac.com>
2024-05-09 10:15:56 +0100

    move common inference method and inference_tspan into common param values

--- pipeline/scripts/common_param_values.jl
+++ pipeline/scripts/common_param_values.jl
@@ -6,6 +6,7 @@
 
       """)
 
+using EpiAware
 # Common generation interval values
 gi_means = [2.0, 10.0, 20.0];
 gi_stds = [2.0];
@@ -13,8 +14,16 @@ gi_stds = [2.0];
 #Common thread count
 num_threads = min(10, Threads.nthreads());
 
-#True Rt
+#Common inference method
+inference_method = EpiMethod(
+    pre_sampler_steps = [ManyPathfinder(nruns = 4, maxiters = 100)],
+    sampler = NUTSampler(adtype = AutoForwardDiff(),
+        ndraws = 2000,
+        nchains = num_threads,
+        mcmc_parallel = MCMCThreads())
+)
 
+#True Rt
 A = 0.3
 P = 30.0
 ϕ = asin(-0.1 / 0.3) * P / (2 * π)
@@ -23,3 +32,6 @@ true_Rt = vcat(fill(1.1, 2 * 7), fill(2.0, 2 * 7), fill(0.5, 2 * 7),
     fill(1.5, 2 * 7), fill(0.75, 2 * 7), fill(1.1, 6 * 7)) |>
           Rt -> [Rt;
                  [1.0 + A * sin.(2 * π * (t - ϕ) / P) for t in 1:(N - length(Rt))]]
+
+# Common inference tspan: Length of true Rt less three weeks
+inference_tspan = (1, N - 21)
