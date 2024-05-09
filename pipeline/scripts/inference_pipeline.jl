d2d0185d19ce8849fcd5c607a4c84dfd6f38f36d
Samuel Brand <sam055@mac.com>
2024-05-09 10:16:21 +0100

    Update inference_pipeline.jl

--- pipeline/scripts/inference_pipeline.jl
+++ pipeline/scripts/inference_pipeline.jl
@@ -1,8 +1,11 @@
 using DrWatson
 @quickactivate "Analysis pipeline"
 
+# Include the AnalysisPipeline module
 include(srcdir("AnalysisPipeline.jl"))
+# Include the common parameter values
 include(scriptsdir("common_param_values.jl"))
+# Include the common inference scenarios
 include(scriptsdir("common_scenarios.jl"))
 
 @info("""
@@ -13,32 +16,31 @@ include(scriptsdir("common_scenarios.jl"))
 
       """)
 
-## Inference method
-# using Distributions, .AnalysisPipeline, EpiAware
+## Inference methods
+using Distributions, .AnalysisPipeline, EpiAware, JLD2, ADTypes, AbstractMCMC
 
+for filename in readdir(datadir("truth_data"))
+    # Load the truth data
+    D = JLD2.load(joinpath(datadir("truth_data"), filename))
 
-num_threads = min(10, Threads.nthreads())
-inference_method = EpiMethod(
-    pre_sampler_steps = [ManyPathfinder(nruns = 4, maxiters = 100)],
-    sampler = NUTSampler(adtype = AutoForwardDiff(),
-        ndraws = 2000,
-        nchains = num_threads,
-        mcmc_parallel = MCMCThreads())
-)
+    # Extract the gi_mean value from the filename
+    _, truth_data_vals = parse_savename(filename)
+    truth_data_gi_mean = truth_data_vals["gi_mean"]
 
-##
-config = sim_configs[end] |>
-    d -> InferenceConfig(d[:igp], d[:latent_model];
-        gi_mean = d[:gi_mean],
-        gi_std = d[:gi_std],
-        case_data = fill(100, 100),
-        tspan = (1, 25),
-        epimethod = inference_method
-    )
+    # Set up and run the inference scenarios
+    for d in sim_configs
+        config = InferenceConfig(d[:igp], d[:latent_model];
+            gi_mean = d[:gi_mean],
+            gi_std = d[:gi_std],
+            case_data = D["y_t"],
+            tspan = inference_tspan,
+            epimethod = inference_method
+        )
+        prfx = "observables" * "_igp_" * string(d[:igp]) * "_latentmodel_" *
+               naming_scheme[d[:latent_model]] * "_truth_gi_mean_" *
+               string(truth_data_gi_mean)
 
-##
-
-inference_results
-
-savename(config)
-config.igp |> string
+        data, file = produce_or_load(
+            simulate_or_infer, config, datadir("epiaware_observables"); prefix = prfx)
+    end
+end
