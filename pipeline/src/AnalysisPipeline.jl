a7058a4a3c3fc6a08a4ce1a34f6faa13165f0c9f
Samuel Brand <sam055@mac.com>
2024-05-10 12:48:45 +0100

    default prior function and unit test

--- pipeline/src/AnalysisPipeline.jl
+++ pipeline/src/AnalysisPipeline.jl
@@ -4,18 +4,19 @@ This module contains the analysis pipeline for the `Rt-without-renewal` project.
 module AnalysisPipeline
 
 using CSV, Dagger, DataFramesMeta, Dates, Distributions, DocStringExtensions, DrWatson,
-      EpiAware, Plots, Statistics
+      EpiAware, Plots, Statistics, ADTypes, AbstractMCMC
 
 # Exported struct types
 export TruthSimulationConfig, InferenceConfig
 
 # Exported functions
-export simulate_or_infer, default_gi_params, default_Rt, default_tspan
+export simulate_or_infer, default_gi_params, default_Rt, default_tspan, default_priors
 
 include("docstrings.jl")
 include("default_gi_params.jl")
 include("default_Rt.jl")
 include("default_tspan.jl")
+include("default_priors.jl")
 include("TruthSimulationConfig.jl")
 include("InferenceConfig.jl")
 
--- pipeline/test/default_returning_functions.jl
+++ pipeline/test/default_returning_functions.jl
@@ -20,3 +20,17 @@ end
     tspan = default_tspan()
     @test tspan isa Tuple{Integer, Integer}
 end
+
+@testset "default_priors: generates a dict with correct keys and distributions" begin
+    using .AnalysisPipeline, Distributions
+    # Call the default_priors function
+    priors_dict = default_priors()
+
+    # Check if the priors dictionary is constructed correctly
+    @test haskey(priors_dict, "transformed_process_init_prior")
+    @test haskey(priors_dict, "std_prior")
+    @test haskey(priors_dict, "damp_param_prior")
+
+    # Check if the values are all distributions
+    @test valtype(priors_dict) <: Distribution
+end
