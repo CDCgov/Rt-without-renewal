26285df9e46ea0464e2b5c0349a26e0b1b999612
Samuel Brand <sam055@mac.com>
2024-05-13 14:07:15 +0100

    update analysis pipeline

--- pipeline/scripts/analysis_pipeline.jl
+++ pipeline/scripts/analysis_pipeline.jl
@@ -7,9 +7,6 @@ using DrWatson
 
 # Include the AnalysisPipeline module
 include(srcdir("AnalysisPipeline.jl"));
-# Include some plotting functionality that is useful but not necessary for analysis
-# pipeline
-include(plotsdir("plot_functions.jl"));
 
 @info("""
       Running the analysis pipeline.
@@ -18,11 +15,9 @@ include(plotsdir("plot_functions.jl"));
       Path of active project: $(projectdir())
       """)
 
-using .AnalysisPipeline
-
 ## Other dependencies
 # Analogy: tar_option_set(...) in targets
-using JLD2, Plots
+using .AnalysisPipeline
 
 ## Run the pipeline steps
 # Analogy: list(tar_targets...) followed by tar_make(...) in targets
@@ -38,46 +33,32 @@ latent_models_names = Dict(value => key for (key, value) in latent_models_dict)
 tspan = default_tspan()
 inference_method = default_inference_method()
 
-# truth data configurations (e.g. different GI means) and inference configurations
-# (e.g. different infection generation processes and latent models etc.)
+# truth data configurations (e.g. different GI means)
 truth_data_configs = make_truth_data_configs(
     gi_means = default_gi_param_dict["gi_means"], gi_stds = default_gi_param_dict["gi_stds"])
+
+# inference configurations
+# (e.g. different infection generation processes and latent models etc.)
 
 inference_configs = make_inference_configs(
     latent_models = collect(values(latent_models_dict)),
     gi_means = default_gi_param_dict["gi_means"],
     gi_stds = default_gi_param_dict["gi_stds"])
 
-truth_data_config_structs = map(truth_data_configs) do d
-    TruthSimulationConfig(
-        truth_process = true_Rt, gi_mean = d["gi_mean"], gi_std = d["gi_std"])
-end
-
 # Produce and save the truth data
-truth_data_scenarios = map(truth_data_config_structs) do truth_data_config_struct
+truth_data_scenarios = map(truth_data_configs[1:1]) do truth_data_config
+    @info "Running truth data scenario with GI mean: $(truth_data_config["gi_mean"])"
     # generate truth data
-    truthdata, truthfile = produce_or_load(
-        simulate_or_infer, truth_data_config_struct, datadir("truth_data"); prefix = "truth_data")
-
-    plot_truth_data(truthdata, truth_data_config_struct)
+    truthdata, truthfile = generate_truthdata_from_config(
+        truth_data_config; plot = true)
 
     # Run the inference scenarios
-    map(inference_configs) do inference_config
-        config = InferenceConfig(inference_config["igp"], inference_config["latent_model"];
-            gi_mean = inference_config["gi_mean"],
-            gi_std = inference_config["gi_std"],
-            case_data = truthdata["y_t"],
-            tspan = tspan,
-            epimethod = inference_method
-        )
-        # produce or load inference results
-
-        prfx = "observables" * "_igp_" * string(inference_config["igp"]) * "_latentmodel_" *
-               latent_models_names[inference_config["latent_model"]] * "_truth_gi_mean_" *
-               string(truth_data_config_struct.gi_mean)
-
-        inference_results, inferencefile = produce_or_load(
-            simulate_or_infer, config, datadir("epiaware_observables"); prefix = prfx)
+    map(inference_configs[1:1]) do inference_config
+        @info "Running inference scenario with IGP: $(inference_config["igp"]), latent model: " *
+              latent_models_names[inference_config["latent_model"]]
+        inference_results, inferencefile = generate_inference_results(
+            truthdata, inference_config; tspan, inference_method,
+            truth_data_config, latent_models_names)
         return nothing
     end
 
--- pipeline/src/AnalysisPipeline.jl
+++ pipeline/src/AnalysisPipeline.jl
@@ -5,26 +5,30 @@ module AnalysisPipeline
 
 using Dates: default
 using CSV, Dagger, DataFramesMeta, Dates, Distributions, DocStringExtensions, DrWatson,
-      EpiAware, Plots, Statistics, ADTypes, AbstractMCMC
+      EpiAware, Plots, Statistics, ADTypes, AbstractMCMC, Plots, JLD2
 
 # Exported struct types
 export TruthSimulationConfig, InferenceConfig
 
 # Exported functions
-export simulate_or_infer, default_gi_params, default_Rt, default_tspan, default_priors,
+export simulate_or_infer, default_gi_params, default_Rt, default_tspan,
+       default_latent_model_priors,
        default_epiaware_models, default_inference_method, make_truth_data_configs,
-       make_inference_configs
+       make_inference_configs, generate_truthdata_from_config, generate_inference_results,
+       plot_truth_data, plot_Rt
 
 include("docstrings.jl")
 include("default_gi_params.jl")
 include("default_Rt.jl")
 include("default_tspan.jl")
-include("default_priors.jl")
+include("default_latent_model_priors.jl")
 include("default_epiaware_models.jl")
 include("default_inference_method.jl")
 include("make_truth_data_configs.jl")
 include("make_inference_configs.jl")
 include("TruthSimulationConfig.jl")
 include("InferenceConfig.jl")
-
+include("generate_truthdata.jl")
+include("generate_inference_results.jl")
+include("plot_functions.jl")
 end
