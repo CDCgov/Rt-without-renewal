a4d9ea6f87e3dbac8e4a2d9d38f2475cce61d46d
Samuel Brand <sam055@mac.com>
2024-05-14 09:38:09 +0100

    Update runtests.jl

--- pipeline/test/runtests.jl
+++ pipeline/test/runtests.jl
@@ -1,12 +1,12 @@
 using DrWatson, Test
-@quickactivate "Analysis pipeline"
+quickactivate(@__DIR__(), "Analysis pipeline")
 
 # Load analysis module
-include(srcdir("AnalysisPipeline.jl"))
+include(srcdir("AnalysisPipeline.jl"));
 
 #run tests
 include("default_returning_functions.jl");
-include("test_make_configs.jl")
+include("test_make_configs.jl");
 include("test_SimulationConfig.jl");
 include("test_TruthSimulationConfig.jl");
 include("test_InferenceConfig.jl");
