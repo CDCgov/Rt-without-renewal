66d14ff285aa02ebbec6821958dde28ca5071d4c
Samuel Brand <sam055@mac.com>
2024-05-15 14:51:28 +0100

    Update generate_inference_results.jl

--- pipeline/src/generate_inference_results.jl
+++ pipeline/src/generate_inference_results.jl
@@ -12,8 +12,8 @@ Generate inference results based on the given configuration of inference model o
 - `inference_results`: The generated inference results.
 - `inferencefile`: The file path where the inference results are stored.
 """
-function generate_inference_results(truthdata, inference_config; tspan, inference_method,
-        truth_data_config, latent_models_names,
+function generate_inference_results(
+        truthdata, inference_config; tspan, inference_method, latent_models_names,
         prfix_name = "observables", datadir_name = "epiaware_observables")
     config = InferenceConfig(inference_config["igp"], inference_config["latent_model"];
         gi_mean = inference_config["gi_mean"],
@@ -22,13 +22,13 @@ function generate_inference_results(truthdata, inference_config; tspan, inferenc
         tspan = tspan,
         epimethod = inference_method
     )
-    # produce or load inference results
 
+    # produce or load inference results
     prfx = prfix_name * "_igp_" * string(inference_config["igp"]) * "_latentmodel_" *
            latent_models_names[inference_config["latent_model"]] * "_truth_gi_mean_" *
-           string(truth_data_config["gi_mean"])
+           string(truthdata["truth_gi_mean"])
 
     inference_results, inferencefile = produce_or_load(
         infer, config, datadir(datadir_name); prefix = prfx)
-    return inference_results, inferencefile
+    return inference_results
 end
