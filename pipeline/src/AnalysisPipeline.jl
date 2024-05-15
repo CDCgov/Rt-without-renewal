ddebc9f79a189af9120f6a8c52d9fe7b3b4c38f3
Samuel Brand <sam055@mac.com>
2024-05-15 10:41:31 +0100

    move default constructors into own folder

--- pipeline/src/AnalysisPipeline.jl
+++ pipeline/src/AnalysisPipeline.jl
@@ -1,5 +1,12 @@
 """
 This module contains the analysis pipeline for the `Rt-without-renewal` project.
+
+# Pipeline Components
+
+In this module the meaning of a _pipeline component_ is a directed-acylic-graph
+(DAG) of tasks defined using `Dagger.jl` via dispatch on an `AbstractEpiAwarePipeline`
+sub-type from a function with prefix `make_`. A full pipeline is a sequence of DAGs,
+with execution determined by available computational resources.
 """
 module AnalysisPipeline
 
@@ -8,7 +15,8 @@ using CSV, Dagger, DataFramesMeta, Dates, Distributions, DocStringExtensions, Dr
       EpiAware, Plots, Statistics, ADTypes, AbstractMCMC, Plots, JLD2
 
 # Exported struct types
-export TruthSimulationConfig, InferenceConfig
+export AbstractEpiAwarePipeline, EpiAwarePipeline, RtwithoutRenewalPipeline,
+       TruthSimulationConfig, InferenceConfig
 
 # Exported functions
 export simulate, infer, default_gi_params, default_Rt, default_tspan,
@@ -17,15 +25,16 @@ export simulate, infer, default_gi_params, default_Rt, default_tspan,
        generate_truthdata_from_config, generate_inference_results, plot_truth_data, plot_Rt
 
 include("docstrings.jl")
-include("default_gi_params.jl")
-include("default_Rt.jl")
-include("default_tspan.jl")
-include("default_latent_model_priors.jl")
-include("default_epiaware_models.jl")
-include("default_inference_method.jl")
+include("pipelinetypes.jl")
+include("default_constructors/default_gi_params.jl")
+include("default_constructors/default_Rt.jl")
+include("default_constructors/default_tspan.jl")
+include("default_constructors/default_latent_model_priors.jl")
+include("default_constructors/default_epiaware_models.jl")
+include("default_constructors/default_inference_method.jl")
 include("make_truth_data_configs.jl")
 include("make_inference_configs.jl")
-include("default_latent_models_names.jl")
+include("default_constructors/default_latent_models_names.jl")
 include("TruthSimulationConfig.jl")
 include("InferenceConfig.jl")
 include("generate_truthdata.jl")
