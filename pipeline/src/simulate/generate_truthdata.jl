ba3678fc7abf295e8f7d5cb6b8bbc21cdf5d244f
Samuel Brand <sam055@mac.com>
2024-05-17 12:17:49 +0100

    fix use of default

--- pipeline/src/simulate/generate_truthdata.jl
+++ pipeline/src/simulate/generate_truthdata.jl
@@ -1,13 +1,13 @@
 """
 Generate truth data from a configuration file. It does this by converting the
 configuration dictionary into a `TruthSimulationConfig` object and then calling
-the `simulate` function to generate the truth data.
+the `simulate` function to generate the truth data. This is the default method.
 
 # Arguments
 - `truth_data_config`: A dictionary containing the configuration parameters for
     generating truth data.
 - `pipeline::AbstractEpiAwarePipeline`: The pipeline object which sets pipeline
-    behavior. This is the default method.
+    behavior.
 - `plot`: A boolean indicating whether to plot the generated truth data. Default
     is `true`.
 - `datadir_str`: A string specifying the directory to save the truth data.
@@ -22,14 +22,14 @@ the `simulate` function to generate the truth data.
 function generate_truthdata(
         truth_data_config, pipeline::AbstractEpiAwarePipeline; plot = true,
         datadir_str = "truth_data", prefix = "truth_data")
-    true_Rt = default_Rt()
+    true_Rt = make_Rt(pipeline)
     config = TruthSimulationConfig(
         truth_process = true_Rt, gi_mean = truth_data_config["gi_mean"],
         gi_std = truth_data_config["gi_std"])
     truthdata, truthfile = produce_or_load(
         simulate, config, datadir(datadir_str); prefix = prefix)
     if plot
-        plot_truth_data(truthdata, config)
+        plot_truth_data(truthdata, config, pipeline)
     end
     return truthdata
 end
