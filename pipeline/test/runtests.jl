eab30a0e290119a677f6265681d95f407dc9b727
Samuel Brand <sam055@mac.com>
2024-05-17 12:19:52 +0100

    Update runtests.jl

--- pipeline/test/runtests.jl
+++ pipeline/test/runtests.jl
@@ -6,7 +6,6 @@ include(srcdir("AnalysisPipeline.jl"));
 
 #run tests
 include("pipeline/test_pipelinetypes.jl");
-include("constructors/default_returning_functions.jl");
 include("constructors/test_constructors.jl");
 include("simulate/test_TruthSimulationConfig.jl");
 include("simulate/test_SimulationConfig.jl");
