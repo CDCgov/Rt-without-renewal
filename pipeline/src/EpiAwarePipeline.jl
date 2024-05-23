c538bc9690f16196a690125b3f55d3d380395bb9
Samuel Brand <sam055@mac.com>
2024-05-23 14:12:58 +0100

    rework generate_forecasts to sample white noise forwards in time

--- pipeline/src/EpiAwarePipeline.jl
+++ pipeline/src/EpiAwarePipeline.jl
@@ -11,7 +11,7 @@ with execution determined by available computational resources.
 module EpiAwarePipeline
 
 using CSV, Dagger, DataFramesMeta, Dates, Distributions, DocStringExtensions, DrWatson,
-      EpiAware, Plots, Statistics, ADTypes, AbstractMCMC, Plots, JLD2
+      EpiAware, Plots, Statistics, ADTypes, AbstractMCMC, Plots, JLD2, MCMCChains
 
 # Exported pipeline types
 export AbstractEpiAwarePipeline, EpiAwarePipeline, RtwithoutRenewalPipeline,
--- pipeline/src/forecast/generate_forecasts.jl
+++ pipeline/src/forecast/generate_forecasts.jl
@@ -10,12 +10,27 @@ Generate forecasts for `n` time steps above based on the given inference results
 
 """
 function generate_forecasts(inference_results, n::Integer)
-    chn = inference_results["inference_results"].samples
+    chn = inference_results["inference_results"].samples |> resetrange
     data = inference_results["inference_results"].data
-    forecast_epiprob = define_forecast_epiprob(inference_results["epiprob"], n)
+    epiprob = inference_results["epiprob"]
+    forecast_epiprob = define_forecast_epiprob(epiprob, n)
+
+    # get length of baseline model latent process
+    n_lat = generate_epiaware(epiprob, (y_t = missing,)) |> rand |>
+            θ -> length(getfield(θ, Symbol("latent.ϵ_t")))
+    # get length of forecast model latent process
+    n_lat_forecast = generate_epiaware(forecast_epiprob, (y_t = missing,)) |> rand |>
+                     θ -> length(getfield(θ, Symbol("latent.ϵ_t")))
+    # Add forward generation of white noise to forecast model chain
+    fwd_chn_names = [Symbol("latent.ϵ_t[$(i)]") for i in (n_lat + 1):n_lat_forecast]
+    fwd_chn = Chains(
+        randn(size(chn, 1), n_lat_forecast - n_lat, size(chn, 3)), fwd_chn_names)
+    pred_chn = hcat(chn, fwd_chn)
+
     forecast_quantities = forecast_epiprob |>
-                          epiprob -> generate_epiaware(epiprob, (y_t = missing,)) |>
-                                     forecast_mdl -> generated_observables(
-        forecast_mdl, data, chn)
+                          forecast_epiprob -> generate_epiaware(
+        forecast_epiprob, (y_t = missing,)) |>
+                                              forecast_mdl -> generated_observables(
+        forecast_mdl, data, pred_chn)
     return forecast_quantities
 end
