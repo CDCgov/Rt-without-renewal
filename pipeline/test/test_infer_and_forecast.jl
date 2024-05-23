c54bf3ace8aff1183a4f5bac97a38f204edf6c26
Samuel Brand <sam055@mac.com>
2024-05-23 14:13:17 +0100

    end-to-end test: generate, infer, forecast, viz

--- pipeline/test/test_infer_and_forecast.jl
+++ pipeline/test/test_infer_and_forecast.jl
@@ -9,7 +9,7 @@ using Test
     prior = RtwithoutRenewalPriorPipeline()
 
     # Set up data generation on a random scenario
-    lookahead = 21
+    lookahead = 60
     n_observation_steps = 28
     tspan_gen = (1, n_observation_steps + lookahead)
     tspan_inf = (1, n_observation_steps)
@@ -23,8 +23,9 @@ using Test
     truth_sampling = generate_inference_results(
         Dict("y_t" => missing, "truth_gi_mean" => 1.5), inference_config,
         prior; tspan = tspan_gen, inference_method = make_inference_method(prior)) |>
-                d -> d["inference_results"]
+                     d -> d["inference_results"]
 
+    #Choose first sample to represent truth data
     truthdata = truth_sampling.generated[1].generated_y_t
 
     plt = scatter(truthdata, xlabel = "t", ylabel = "y_t", label = "truth data")
@@ -39,7 +40,7 @@ using Test
     @test inference_results["inference_results"] isa EpiAwareObservables
 
     # Make 21-day forecast
-    forecast_quantities = generate_forecasts(inference_results, 60)
+    forecast_quantities = generate_forecasts(inference_results, lookahead)
     @test forecast_quantities isa EpiAwareObservables
 
     # Make forecast spaghetti plot
@@ -51,8 +52,10 @@ using Test
             quantile(row, q)
         end
     end
-    plot!(plt, forecast_qs, label = "forecast quantiles", color = :grey, lw = [0.5 1.5 3 1.5 0.5])
+    plot!(plt, forecast_qs, label = "forecast quantiles",
+        color = :grey, lw = [0.5 1.5 3 1.5 0.5])
     plot!(plt, ylims = (-0.5, maximum(truthdata) * 1.25))
+    display(plt)
 
     # Make forecast plot for Z_t
     infer_Z_t = mapreduce(hcat, inference_results["inference_results"].generated) do gen
@@ -61,9 +64,13 @@ using Test
     forecast_Z_t = mapreduce(hcat, forecast_quantities.generated) do gen
         gen.Z_t
     end
-    plt_Zt = plot(truth_sampling.generated[1].Z_t, lw = 3, color = :black, label = "truth Z_t")
-    plot!(plt_Zt, infer_Z_t, xlabel = "t", ylabel = "Z_t", label = "", color = :grey, alpha = 0.05)
-    plot!((n_observation_steps+1):size(forecast_Z_t, 1), forecast_Z_t[(n_observation_steps+1):end,:], label = "", color = :red, alpha = 0.05)
+    plt_Zt = plot(
+        truth_sampling.generated[1].Z_t, lw = 3, color = :black, label = "truth Z_t")
+    plot!(plt_Zt, infer_Z_t, xlabel = "t", ylabel = "Z_t",
+        label = "", color = :grey, alpha = 0.05)
+    plot!((n_observation_steps + 1):size(forecast_Z_t, 1),
+        forecast_Z_t[(n_observation_steps + 1):end, :],
+        label = "", color = :red, alpha = 0.05)
     vline!(plt_Zt, [n_observation_steps], label = "forecast start")
 
     display(plt_Zt)
