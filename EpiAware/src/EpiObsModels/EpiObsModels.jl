1506b5212fd89031f22ceaca5243066c5047c6d0
Sam <s.e.abbott12@gmail.com>
2024-06-03 23:33:44 +0100

    add a test for ascertainment_dayofweek and makes EpiObsModels depend on EpiLatentModels

--- EpiAware/src/EpiObsModels/EpiObsModels.jl
+++ EpiAware/src/EpiObsModels/EpiObsModels.jl
@@ -7,9 +7,14 @@ using ..EpiAwareBase
 
 using ..EpiAwareUtils: censored_pmf, HalfNormal
 
+using ..EpiLatentModels: HierarchicalNormal, broadcast_dayofweek
+
 using Turing, Distributions, DocStringExtensions, SparseArrays
 
 export PoissonError, NegativeBinomialError, LatentDelay, Ascertainment
+
+# helper functions
+export ascertainment_dayofweek
 
 include("docstrings.jl")
 include("LatentDelay.jl")
