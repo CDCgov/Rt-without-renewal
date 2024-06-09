d47c1baeac8fa470fd365126475afca5b520a159
Sam <s.e.abbott12@gmail.com>
2024-06-09 23:52:03 +0100

    add ObservationErrorModel type and functions

---
+++ EpiAware/src/EpiObsModels/ObservationErrorModels/AbstractTuringObservationModel.jl
@@ -0,0 +1,30 @@
+abstract type AbstractTuringObservationErrorModel <: AbstractTuringObservationModel end
+
+@model function EpiAwareBase.generate_observations(
+        obs_model::AbstractTuringObservationErrorModel,
+        y_t,
+        Y_t)
+    @submodel priors = generate_obs_error_priors(obs_model, y_t, Y_t)
+
+    if ismissing(y_t)
+        y_t = Vector{Int}(undef, length(Y_t))
+    end
+
+    Y_y = length(y_t) - length(Y_t)
+
+    for i in eachindex(Y_t)
+        y_t[Y_y + i] ~ obs_error(obs_model, Y_t[i] + 1e-6)
+    end
+
+    return y_t, priors
+end
+
+@model function generate_obs_error_priors(
+        obs_model::AbstractTuringObservationErrorModel, y_t, Y_t)
+    return NamedTuple()
+end
+
+function obs_error(obs_model::AbstractTuringObservationErrorModel, Y_t)
+    @info "No concrete implementation for `_apply_method` is defined."
+    return nothing
+end
