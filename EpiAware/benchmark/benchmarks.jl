11feb0eb0732ef9f479ca281448da7186f2c3985
Sam <s.e.abbott12@gmail.com>
2024-06-19 12:28:40 +0100

    add a proposed structure for future benchmarks

--- EpiAware/benchmark/benchmarks.jl
+++ EpiAware/benchmark/benchmarks.jl
@@ -1,7 +1,11 @@
 using BenchmarkTools
 SUITE = BenchmarkGroup()
-for file in readdir(joinpath(@__DIR__, "bench"))
-    if endswith(file, ".jl")
-        SUITE[basename(file, ".jl")] = include(joinpath(@__DIR__, "bench", file))
+for folder in readdir(joinpath(@__DIR__, "bench"))
+    if isdir(joinpath(@__DIR__, "bench", folder))
+        suite_name = basename(folder)
+        suite_file = joinpath(@__DIR__, "bench", folder, "$suite_name.jl")
+        if isfile(suite_file)
+            SUITE[suite_name] = include(suite_file)
+        end
     end
 end
