6e89df7cda34476c67cddf8df379eeaf61cd6974
Samuel Brand <sam055@mac.com>
2024-07-03 11:17:28 +0100

    format fix

--- pipeline/src/infer/inference_prefix.jl
+++ pipeline/src/infer/inference_prefix.jl
@@ -3,48 +3,52 @@ This is an internal method that generates the prefix for the inference results f
 """
 function _inference_prefix(truthdata, inference_config, pipeline::AbstractEpiAwarePipeline)
     return "observables" * "_igp_" * string(inference_config["igp"]) * "_latentmodel_" *
-        inference_config["latent_namemodels"].first * "_truth_gi_mean_" *
-        string(truthdata["truth_gi_mean"]) * "_used_gi_mean_" *
-        string(inference_config["gi_mean"])
+           inference_config["latent_namemodels"].first * "_truth_gi_mean_" *
+           string(truthdata["truth_gi_mean"]) * "_used_gi_mean_" *
+           string(inference_config["gi_mean"])
 end
 
 function _inference_prefix(truthdata, inference_config, pipeline::EpiAwareExamplePipeline)
-    return "testmode_observables" * "_igp_" * string(inference_config["igp"]) * "_latentmodel_" *
-        inference_config["latent_namemodels"].first * "_truth_gi_mean_" *
-        string(truthdata["truth_gi_mean"]) * "_used_gi_mean_" *
-        string(inference_config["gi_mean"])
+    return "testmode_observables" * "_igp_" * string(inference_config["igp"]) *
+           "_latentmodel_" *
+           inference_config["latent_namemodels"].first * "_truth_gi_mean_" *
+           string(truthdata["truth_gi_mean"]) * "_used_gi_mean_" *
+           string(inference_config["gi_mean"])
 end
 
-function _inference_prefix(truthdata, inference_config, pipeline::RtwithoutRenewalPriorPipeline)
-    return "prior_observables" * "_igp_" * string(inference_config["igp"]) * "_latentmodel_" *
-        inference_config["latent_namemodels"].first * "_truth_gi_mean_" *
-        string(inference_config["gi_mean"])
+function _inference_prefix(
+        truthdata, inference_config, pipeline::RtwithoutRenewalPriorPipeline)
+    return "prior_observables" * "_igp_" * string(inference_config["igp"]) *
+           "_latentmodel_" *
+           inference_config["latent_namemodels"].first * "_truth_gi_mean_" *
+           string(inference_config["gi_mean"])
 end
 
 function _inference_prefix(truthdata, inference_config, pipeline::SmoothOutbreakPipeline)
     return "smooth_outbreak" * "_igp_" * string(inference_config["igp"]) * "_latentmodel_" *
-        inference_config["latent_namemodels"].first * "_truth_gi_mean_" *
-        string(truthdata["truth_gi_mean"]) * "_used_gi_mean_" *
-        string(inference_config["gi_mean"])
+           inference_config["latent_namemodels"].first * "_truth_gi_mean_" *
+           string(truthdata["truth_gi_mean"]) * "_used_gi_mean_" *
+           string(inference_config["gi_mean"])
 end
 
 function _inference_prefix(truthdata, inference_config, pipeline::MeasuresOutbreakPipeline)
-    return "measures_outbreak" * "_igp_" * string(inference_config["igp"]) * "_latentmodel_" *
-        inference_config["latent_namemodels"].first * "_truth_gi_mean_" *
-        string(truthdata["truth_gi_mean"]) * "_used_gi_mean_" *
-        string(inference_config["gi_mean"])
+    return "measures_outbreak" * "_igp_" * string(inference_config["igp"]) *
+           "_latentmodel_" *
+           inference_config["latent_namemodels"].first * "_truth_gi_mean_" *
+           string(truthdata["truth_gi_mean"]) * "_used_gi_mean_" *
+           string(inference_config["gi_mean"])
 end
 
 function _inference_prefix(truthdata, inference_config, pipeline::SmoothEndemicPipeline)
     return "smooth_endemic" * "_igp_" * string(inference_config["igp"]) * "_latentmodel_" *
-        inference_config["latent_namemodels"].first * "_truth_gi_mean_" *
-        string(truthdata["truth_gi_mean"]) * "_used_gi_mean_" *
-        string(inference_config["gi_mean"])
+           inference_config["latent_namemodels"].first * "_truth_gi_mean_" *
+           string(truthdata["truth_gi_mean"]) * "_used_gi_mean_" *
+           string(inference_config["gi_mean"])
 end
 
 function _inference_prefix(truthdata, inference_config, pipeline::RoughEndemicPipeline)
     return "rough_endemic" * "_igp_" * string(inference_config["igp"]) * "_latentmodel_" *
-        inference_config["latent_namemodels"].first * "_truth_gi_mean_" *
-        string(truthdata["truth_gi_mean"]) * "_used_gi_mean_" *
-        string(inference_config["gi_mean"])
+           inference_config["latent_namemodels"].first * "_truth_gi_mean_" *
+           string(truthdata["truth_gi_mean"]) * "_used_gi_mean_" *
+           string(inference_config["gi_mean"])
 end
