21e093ae10128490a554ee556fffd4817b8ec199
Sam <s.e.abbott12@gmail.com>
2024-07-05 11:24:28 +0100

    remove failing doc test for now

--- EpiAware/src/EpiAwareUtils/get_param_array.jl
+++ EpiAware/src/EpiAwareUtils/get_param_array.jl
@@ -14,7 +14,7 @@ MCMC sample as a `NamedTuple`.
 Sampling from a simple model which has both scalar and vector quantity random variables across
 4 chains.
 
-```jldoctest; filter = r\"\b\d+(\.\d+)?\b\" => \"*\"
+```julia
 using Turing, MCMCChains, EpiAware
 
 @model function testmodel()
@@ -24,10 +24,6 @@ mdl = testmodel()
 chn = sample(mdl, Prior(), MCMCSerial(), 2, 1, progress=false)
 
 A = get_param_array(chn)
-# output
-2×1 Matrix{@NamedTuple{iteration::Int64, chain::Int64, y::Float64, lp::Float64}}:
- (iteration = 1, chain = 1, y = 1.2817564703577078, lp = -1.7403883578565975)
- (iteration = 2, chain = 1, y = 0.7259423762267098, lp = -1.1824347000055138)
 ```
 "
 function get_param_array(chn::Chains)
