7a76934efe427f5425a90d5d73d48d98faa1594d
Samuel Brand <sam055@mac.com>
2024-07-05 15:30:15 +0100

    Update make_tspan.jl

--- pipeline/src/constructors/make_tspan.jl
+++ pipeline/src/constructors/make_tspan.jl
@@ -1,16 +1,24 @@
 """
-Constructs the time span for the given `pipeline` object.
+Constructs a time span for performing inference on a case data time series. This
+    is the default method.
 
 # Arguments
-- `pipeline::AbstractEpiAwarePipeline`: The pipeline object for which the time
-    span is constructed. This is the default method.
+- `pipeline::AbstractEpiAwarePipeline`: The pipeline object used for analysis.
+- `T::Union{Integer,Nothing} = nothing`: The `stop` point at which to construct
+    the time span. If `nothing`, the time span will be constructed using the
+    length of the Rt vector for `pipeline`.
+- `lookback = 35`: The number of days to look back from the specified time point.
 
 # Returns
-- `tspan::Tuple{Float64, Float64}`: The time span as a tuple of start and end times.
+A tuple `(start, stop)` representing the start and stop indices of the time span.
+
+# Examples
 
 """
-function make_tspan(pipeline::AbstractEpiAwarePipeline; backhorizon = 21)
+function make_tspan(pipeline::AbstractEpiAwarePipeline;
+        T::Union{Integer, Nothing} = nothing, lookback = 35)
     N = size(make_Rt(pipeline), 1)
+    _T = isnothing(T) ? N : T
     @assert backhorizon<N "Backhorizon must be less than the length of the default Rt."
-    return (1, N - backhorizon)
+    return (max(1, _T - lookback), min(N, _T))
 end
