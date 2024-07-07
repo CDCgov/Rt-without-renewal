d2cec09ea42f73641ab83dfffbc156d9c85b9e3a
Sam <s.e.abbott12@gmail.com>
2024-07-07 20:17:43 +0100

    add benchmarking for observation error generic methods

--- benchmark/bench/EpiObsModels/ObservationErrorModels/methods.jl
+++ benchmark/bench/EpiObsModels/ObservationErrorModels/methods.jl
@@ -1,29 +1,31 @@
-@testitem "Test specific generate_observations" begin
-    using Distributions: Normal
+suite["observation_error"] = BenchmarkGroup()
+
+let
+    using Distributions, Turing
+
     struct TestObs <: AbstractTuringObservationErrorModel end
 
-    function EpiObsModels.observation_error(model::TestObs, Y_t)
-        Normal(Y_t, 1e-6)
+    function EpiObsModels.observation_error(model::TestObs, Y_t, std)
+        Normal(Y_t, std)
+    end
+
+    @model function EpiObsModels.generate_observation_error_priors(model::TestObs, y_t, Y_t)
+        std ~ truncated(Normal(0.0, 1.0), 0.0, 1.0)
+        return (std,)
     end
 
     obs_model = TestObs()
 
     I_t = [10.0, 20.0, 30.0, 40.0, 50.0]
+    mdl = generate_observations(obs_model, missing, I_t)
 
-    @testset "Test with entirely missing data" begin
-        mdl = generate_observations(obs_model, missing, I_t)
-        @test isapprox(mdl()[1], I_t, atol = 1e-3)
-    end
+    suite["observation_error"]["missing obs"] = make_turing_suite(mdl; check = false)
 
     missing_I_t = vcat(missing, I_t)
+    mdl2 = generate_observations(obs_model, missing_I_t, vcat(20, I_t))
+    suite["observation_error"]["partially missing obs"] = make_turing_suite(
+        mdl2; check = false)
 
-    @testset "Test with leading missing expected observations" begin
-        mdl = generate_observations(obs_model, missing_I_t, vcat(20, I_t))
-        draw = mdl()[1]
-        @test draw[2:end] == I_t
-        @test abs(draw[1] - 20) > 0
-        @test isapprox(draw[1], 20, atol = 1e-3)
-    end
-
-    @test_throws AssertionError generate_observations(obs_model, vcat(1, I_t), I_t)()
+    mdl3 = generate_observations(obs_model, I_t, I_t)
+    suite["observation_error"]["no missing obs"] = make_turing_suite(mdl3; check = true)
 end
