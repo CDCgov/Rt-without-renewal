8e83a3036e1d589d2ea0c70f721aa4c694b36ebe
Samuel Brand <sam055@mac.com>
2024-07-08 15:51:47 +0100

    Method for multiple pipelines

--- pipeline/src/pipeline/do_pipeline.jl
+++ pipeline/src/pipeline/do_pipeline.jl
@@ -2,7 +2,7 @@
 Create a pipeline by generating truth data and making inferences.
 
 # Arguments
-- `pipeline::AbstractEpiAwarePipeline`: The pipeline object which sets pipeline behavior.
+- `pipeline::AbstractEpiAwarePipeline`: The pipeline object which sets pipeline behavior and scenario.
 
 """
 function do_pipeline(pipeline::AbstractEpiAwarePipeline)
@@ -10,5 +10,17 @@ function do_pipeline(pipeline::AbstractEpiAwarePipeline)
     for truthdata in truthdatas
         do_inference(truthdata, pipeline)
     end
+    return nothing
+end
+
+"""
+Perform the pipeline for each `AbstractEpiAwarePipeline` in the given vector `pipelines`.
+
+# Arguments
+- `pipelines`: A vector of `AbstractEpiAwarePipeline` objects.
+
+"""
+function do_pipeline(pipelines::Vector{<:AbstractEpiAwarePipeline})
+    map(do_pipeline, pipelines)
     return nothing
 end
