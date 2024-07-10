fc8f17128711044ee5a1333c05f8f83dab5a27e7
Samuel Brand <sam055@mac.com>
2024-07-10 14:26:47 +0100

    Fix inference prefixing along with a method refactor

--- pipeline/src/infer/inference_prefix.jl
+++ pipeline/src/infer/inference_prefix.jl
@@ -1,4 +1,27 @@
 """
+This is an internal method that generates part of the prefix for the inference
+    results file name from the truth data and inference configuration.
+"""
+function _prefix_from_config(truthdata, inference_config)
+    igp_str = string(inference_config["igp"]) |> str -> split(str, ".")[end]
+
+    return "_igp_" * igp_str * "_latentmodel_" *
+           inference_config["latent_namemodels"].first * "_truth_gi_mean_" *
+           string(truthdata["truth_gi_mean"]) * "_inference_T_" *
+           string(inference_config["T"])
+end
+
+"""
+This is an internal method that generates the part of the prefix for the inference
+    results file name from the pipeline.
+"""
+_prefix_from_pipeline(pipeline::AbstractEpiAwarePipeline) = "observables"
+_prefix_from_pipeline(pipeline::SmoothOutbreakPipeline) = "smooth_outbreak"
+_prefix_from_pipeline(pipeline::MeasuresOutbreakPipeline) = "measures_outbreak"
+_prefix_from_pipeline(pipeline::SmoothEndemicPipeline) = "smooth_endemic"
+_prefix_from_pipeline(pipeline::RoughEndemicPipeline) = "rough_endemic"
+
+"""
 This is an internal method that generates the prefix for the inference results file name.
 """
 function _inference_prefix(truthdata, inference_config, pipeline::AbstractEpiAwarePipeline)
@@ -24,31 +47,12 @@ function _inference_prefix(
            string(inference_config["gi_mean"])
 end
 
-function _inference_prefix(truthdata, inference_config, pipeline::SmoothOutbreakPipeline)
-    return "smooth_outbreak" * "_igp_" * string(inference_config["igp"]) * "_latentmodel_" *
-           inference_config["latent_namemodels"].first * "_truth_gi_mean_" *
-           string(truthdata["truth_gi_mean"]) * "_used_gi_mean_" *
-           string(inference_config["gi_mean"])
-end
-
-function _inference_prefix(truthdata, inference_config, pipeline::MeasuresOutbreakPipeline)
-    return "measures_outbreak" * "_igp_" * string(inference_config["igp"]) *
-           "_latentmodel_" *
-           inference_config["latent_namemodels"].first * "_truth_gi_mean_" *
-           string(truthdata["truth_gi_mean"]) * "_used_gi_mean_" *
-           string(inference_config["gi_mean"])
-end
-
-function _inference_prefix(truthdata, inference_config, pipeline::SmoothEndemicPipeline)
-    return "smooth_endemic" * "_igp_" * string(inference_config["igp"]) * "_latentmodel_" *
-           inference_config["latent_namemodels"].first * "_truth_gi_mean_" *
-           string(truthdata["truth_gi_mean"]) * "_used_gi_mean_" *
-           string(inference_config["gi_mean"])
-end
-
-function _inference_prefix(truthdata, inference_config, pipeline::RoughEndemicPipeline)
-    return "rough_endemic" * "_igp_" * string(inference_config["igp"]) * "_latentmodel_" *
-           inference_config["latent_namemodels"].first * "_truth_gi_mean_" *
-           string(truthdata["truth_gi_mean"]) * "_used_gi_mean_" *
-           string(inference_config["gi_mean"])
+"""
+This is an internal method that generates the prefix for the inference results file name for
+    `pipeline` objects of type `AbstractRtwithoutRenewalPipeline`.
+"""
+function _inference_prefix(
+        truthdata, inference_config, pipeline::AbstractRtwithoutRenewalPipeline)
+    return _prefix_from_pipeline(pipeline) *
+           _prefix_from_config(truthdata, inference_config)
 end
