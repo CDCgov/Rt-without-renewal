c9d14fc7c65879e41df11d84900b7023550a31ab
Samuel Brand <sam055@mac.com>
2024-07-15 17:15:50 +0100

    scripts for gathering samples into dataframe

--- pipeline/scripts/create_prediction_dataframe.jl
+++ pipeline/scripts/create_prediction_dataframe.jl
@@ -8,15 +8,26 @@ using EpiAwarePipeline, EpiAware, AlgebraOfGraphics, JLD2, DrWatson, Plots, Data
 files = readdir(datadir("epiaware_observables")) |>
         strs -> filter(s -> occursin("jld2", s), strs)
 
-##
+## Define scenarios
 pipelines = [
     SmoothOutbreakPipeline(), MeasuresOutbreakPipeline(),
     SmoothEndemicPipeline(), RoughEndemicPipeline()]
 
+## Set up EpiData objects: Used in the prediction dataframe for infection generating
+## processes that don't use directly in simulation.
+gi_params = make_gi_params(pipelines[1])
+epi_datas = map(gi_params["gi_means"]) do μ
+    σ = gi_params["gi_stds"][1]
+    shape = (μ / σ)^2
+    scale = σ^2 / μ
+    Gamma(shape, scale)
+end .|> gen_dist -> EpiData(gen_distribution = gen_dist)
 
+## Calculate the prediction dataframe
 prediction_df = mapreduce(vcat, files) do filename
     output = load(joinpath(datadir("epiaware_observables"), filename))
     make_prediction_dataframe_from_output(filename, output, epi_datas, pipelines)
 end
 
+## Save the prediction dataframe
 CSV.write(plotsdir("analysis_df.csv"), prediction_df)
