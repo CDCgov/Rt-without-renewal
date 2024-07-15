f71e5d39a5856077569273526f7b9120ea1ace02
Samuel Brand <48288458+SamuelBrand1@users.noreply.github.com>
2024-07-15 23:41:37 +0100

    add parameter recovery for Random walk model

--- EpiAware/test/EpiLatentModels/models/RandomWalk.jl
+++ EpiAware/test/EpiLatentModels/models/RandomWalk.jl
@@ -38,3 +38,91 @@ end
     @test rw_process.init_prior == init_prior
     @test rw_process.std_prior == std_prior
 end
+
+@testitem "Testing RandomWalk parameter recovery: Normal errors" begin
+    using Random, Turing, FillArrays, Distributions, LinearAlgebra, DynamicPPL, StatsBase
+    Random.seed!(1234)
+
+    rw_process = RandomWalk()
+
+    @model function test_normal_errors(rw, Z_obs, σ_obs_prior)
+        n = ismissing(Z_obs) ? 40 : length(Z_obs)
+        σ ~ σ_obs_prior
+        @submodel Z_t, _ = generate_latent(rw, n)
+        Z_obs ~ MvNormal(Z_t, Diagonal(Fill(σ, n)))
+        return Z_t
+    end
+
+    Z_generated = rand(test_normal_errors(rw_process, missing, HalfNormal(0.05)))
+    mdl = test_normal_errors(rw_process, Z_generated.Z_obs, HalfNormal(0.05))
+    chn = sample(mdl, NUTS(), 1000)
+
+    #Posterior predictive p values for the standard deviations
+    #Check that are in central 99% of the posterior predictive distribution
+    posterior_p_σ = ecdf(chn[:σ][:])(Z_generated.σ)
+    posterior_p_σ_RW = ecdf(chn[:σ_RW][:])(Z_generated.σ_RW)
+
+    @test 0.005 < posterior_p_σ < 0.9995
+    @test 0.005 < posterior_p_σ_RW < 0.9995
+end
+
+@testitem "Testing RandomWalk parameter recovery: Negative Binomial errors" begin
+    using Random, Turing, FillArrays, Distributions, LinearAlgebra, DynamicPPL, StatsBase
+    Random.seed!(1234)
+
+    rw_process = RandomWalk()
+
+    @model function test_normal_errors(rw, Z_obs, σ_obs_prior)
+        n = ismissing(Z_obs) ? 40 : length(Z_obs)
+        σ ~ σ_obs_prior
+        @submodel Z_t, _ = generate_latent(rw, n)
+        Z_obs ~ MvNormal(Z_t, Diagonal(Fill(σ, n)))
+        return Z_t
+    end
+
+    Z_generated = rand(test_normal_errors(rw_process, missing, HalfNormal(0.05)))
+    mdl = test_normal_errors(rw_process, Z_generated.Z_obs, HalfNormal(0.05))
+    chn = sample(mdl, NUTS(), 1000, progess = false)
+
+    #Posterior predictive p values for the standard deviations
+    #Check that are in central 99% of the posterior predictive distribution
+    posterior_p_σ = ecdf(chn[:σ][:])(Z_generated.σ)
+    posterior_p_σ_RW = ecdf(chn[:σ_RW][:])(Z_generated.σ_RW)
+    posterior_p_rinit = ecdf(chn[:rw_init][:])(obs_rand.rw_init)
+
+    @test 0.005 < posterior_p_σ < 0.9995
+    @test 0.005 < posterior_p_σ_RW < 0.9995
+    @test 0.005 < posterior_p_rinit < 0.9995
+end
+
+@testitem "Testing RandomWalk parameter recovery: Negative Binomial errors on log rw" begin
+    using Random, Turing, FillArrays, Distributions, LinearAlgebra, DynamicPPL, StatsBase
+    Random.seed!(1234)
+
+    rw_process = RandomWalk()
+    obs_nb = NegativeBinomialError()
+
+    @model function test_negbin_errors(rw, obs, y_t)
+        n = ismissing(y_t) ? 40 : length(y_t)
+        @submodel Z_t, _ = generate_latent(rw, n)
+        @submodel y_t, _ = generate_observations(obs, y_t, exp.(Z_t))
+        return Z_t, y_t
+    end
+
+    gen_mdl = test_negbin_errors(rw_process, obs_nb, missing)
+    obs_rand = rand(gen_mdl)
+    Z_t_obs, y_t_obs = condition(gen_mdl, obs_rand)()
+
+    mdl = test_negbin_errors(rw_process, obs_nb, y_t_obs)
+    chn = sample(mdl, NUTS(), 1000, progess = false)
+
+    #Posterior predictive p values for singleton parameters
+    #Check that are in central 99% of the posterior predictive distribution
+    posterior_p_σ_RW = ecdf(chn[:σ_RW][:])(obs_rand.σ_RW)
+    posterior_p_cf = ecdf(chn[:cluster_factor][:])(obs_rand.cluster_factor)
+    posterior_p_rinit = ecdf(chn[:rw_init][:])(obs_rand.rw_init)
+
+    @test 0.005 < posterior_p_cf < 0.9995
+    @test 0.005 < posterior_p_σ_RW < 0.9995
+    @test 0.005 < posterior_p_rinit < 0.9995
+end
