2e30981d0b0d58f20d7a325517ab15c6cf416ada
Samuel Brand <48288458+SamuelBrand1@users.noreply.github.com>
2024-07-28 14:30:34 +0100

    Dataframe constructor script for scoring (#403)
    * fix test
    
    * breakup scoring into more re-usable atomic functions
    
    * make scoring dataframe function
    
    * remove unused kwarg
    
    * modify dataframe constructor script
    
    * Update create_analysis_dataframes.jl
    
    * Update create_analysis_dataframes.jl
    
    ---------
    
    Co-authored-by: Sam Abbott <azw1@cdc.gov>

--- pipeline/src/scoring/summarise_crps.jl
+++ pipeline/src/scoring/summarise_crps.jl
@@ -9,9 +9,8 @@ Summarizes the Continuous Ranked Probability Score (CRPS) for different processe
 A dictionary containing the summarized CRPS scores for different processes.
 
 """
-function summarise_crps(config, inference_results, forecast_results, epiprob)
+function summarise_crps(config, inference_results, forecast_results, epidata)
     ts = config.tspan[1]:min(config.tspan[2] + config.lookahead, length(config.truth_I_t))
-    epidata = epiprob.epi_model.data
 
     procs_names = (:log_I_t, :rt, :Rt, :I_t, :log_Rt)
     scores_log_I_t, scores_rt, scores_Rt, scores_I_t, scores_log_Rt = _process_crps_scores(
@@ -23,6 +22,26 @@ function summarise_crps(config, inference_results, forecast_results, epiprob)
         "scores_rt" => scores_rt, "scores_Rt" => scores_Rt,
         "scores_I_t" => scores_I_t, "scores_log_Rt" => scores_log_Rt,
         "scores_y_t" => scores_y_t, "scores_log_y_t" => scores_log_y_t)
+end
+
+function _get_predicted_proc(inference_results, forecast_results, epidata, process)
+    gens = forecast_results.generated
+    log_I0s = inference_results.samples[:init_incidence]
+    predicted_proc = mapreduce(hcat, gens, log_I0s) do gen, logI0
+        I0 = exp(logI0)
+        It = gen.I_t
+        procs = calculate_processes(It, I0, epidata)
+        getfield(procs, process)
+    end
+    return predicted_proc
+end
+
+function _get_predicted_y_t(forecast_results)
+    gens = forecast_results.generated
+    predicted_y_t = mapreduce(hcat, gens) do gen
+        gen.generated_y_t
+    end
+    return predicted_y_t
 end
 
 """
@@ -37,14 +56,8 @@ function _process_crps_scores(
             config.truth_I_t[ts], true_Itminusone, epidata) |>
                     procs -> getfield(procs, process)
         # predictions
-        gens = forecast_results.generated
-        log_I0s = inference_results.samples[:init_incidence]
-        predicted_proc = mapreduce(hcat, gens, log_I0s) do gen, logI0
-            I0 = exp(logI0)
-            It = gen.I_t
-            procs = calculate_processes(It, I0, epidata)
-            getfield(procs, process)
-        end
+        predicted_proc = _get_predicted_proc(
+            inference_results, forecast_results, epidata, process)
         scores = [simple_crps(preds, true_proc[t])
                   for (t, preds) in enumerate(eachrow(predicted_proc))]
         return scores
@@ -57,10 +70,7 @@ Internal method for calculating the CRPS scores for observed cases and log(cases
 """
 function _cases_crps_scores(forecast_results, config, ts; jitter = 1e-6)
     true_y_t = config.case_data[ts]
-    gens = forecast_results.generated
-    predicted_y_t = mapreduce(hcat, gens) do gen
-        gen.generated_y_t
-    end
+    predicted_y_t = _get_predicted_y_t(forecast_results)
     scores_y_t = [simple_crps(preds, true_y_t[t])
                   for (t, preds) in enumerate(eachrow(predicted_y_t))]
     scores_log_y_t = [simple_crps(log.(preds .+ jitter), log(true_y_t[t] + jitter))
