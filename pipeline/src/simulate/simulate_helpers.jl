390090e02c2c8b587c3ef192f63e29fe82d10836
Samuel Brand <sam055@mac.com>
2024-08-17 12:57:34 +0100

    change filename

---
+++ pipeline/src/simulate/simulate_helpers.jl
@@ -0,0 +1,15 @@
+"""
+Internal method for setting the prefix for the truth data file name.
+"""
+_simulate_prefix(pipeline::AbstractEpiAwarePipeline) = "truth_data"
+function _simulate_prefix(pipeline::AbstractRtwithoutRenewalPipeline)
+    "truth_data_" * pipeline.prefix
+end
+
+"""
+Internal method for setting the data directory path for the truth data.
+"""
+_get_datadir_str(pipeline::AbstractEpiAwarePipeline) = "truth_data"
+function _get_datadir_str(pipeline::AbstractRtwithoutRenewalPipeline)
+    pipeline.testmode ? mktempdir() : "truth_data"
+end
