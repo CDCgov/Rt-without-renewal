4b0bbc6184aed20d4536d57ac0dc2341038ac195
Samuel Brand <sam055@mac.com>
2024-08-26 09:59:11 +0100

    Add @suppress macro to suppress print out into Test results CI

--- EpiAware/test/EpiObsModels/modifiers/LatentDelay.jl
+++ EpiAware/test/EpiObsModels/modifiers/LatentDelay.jl
@@ -253,10 +253,10 @@ end
         return θ_true, gen_data, chn, generative_mdl
     end
 
-    @testset "Check true parameters are within 99.9% central post. prob.: " begin
+    @testset "Check true parameters are within 99% central post. prob.: " begin
         @testset for latentprocess_type in latentprocess_types, epimodel in epimodels
             latentprocess = set_latent_process(epimodel, latentprocess_type)
-            _ = test_full_process(epimodel, latentprocess, 50)
+            @suppress _ = test_full_process(epimodel, latentprocess, 50)
         end
     end
 end
