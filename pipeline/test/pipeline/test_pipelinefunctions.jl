bc712e924b3c413d480331172214bb9eb49a8a56
Samuel Brand <sam055@mac.com>
2024-08-30 09:10:36 +0100

    unit tests and end-to-end tests

--- pipeline/test/pipeline/test_pipelinefunctions.jl
+++ pipeline/test/pipeline/test_pipelinefunctions.jl
@@ -1,28 +1,49 @@
 @testset "do_truthdata tests" begin
     using EpiAwarePipeline, Dagger
-    pipeline = EpiAwareExamplePipeline()
-    truthdata_dg_task = do_truthdata(pipeline)
-    truthdata = fetch.(truthdata_dg_task)
+    for pipetype in [SmoothOutbreakPipeline, MeasuresOutbreakPipeline,
+                     SmoothEndemicPipeline, RoughEndemicPipeline]
+        pipeline = pipetype(; testmode = true)
+        truthdata_dg_task = do_truthdata(pipeline)
+        truthdata = fetch.(truthdata_dg_task)
 
-    @test length(truthdata) == 1
-    @test all([data["y_t"] isa Vector{Union{Missing, T}} where {T <: Integer}
-               for data in truthdata])
+        @test length(truthdata) == 1
+        @test all([data["y_t"] isa Vector{Union{Missing, T}} where {T <: Integer}
+                   for data in truthdata])
+    end
 end
 
 @testset "do_inference tests" begin
     using EpiAwarePipeline, Dagger, EpiAware
-    pipeline = EpiAwareExamplePipeline()
 
-    function make_inference()
-        truthdata = do_truthdata(pipeline)
+    function make_inference(pipeline)
+        truthdata_dg_task = do_truthdata(pipeline)
+        truthdata = fetch.(truthdata_dg_task)
         do_inference(truthdata[1], pipeline)
     end
 
-    inference_results_tsk = make_inference()
+    pipetype = SmoothOutbreakPipeline
+    pipeline = pipetype(;ndraws = 40, testmode = true)
+    truthdata_dg_task = do_truthdata(pipeline)
+    truthdata = fetch.(truthdata_dg_task)
+    inference_configs = make_inference_configs(pipeline)
+    inference_method = make_inference_method(pipeline)
+    map(inference_configs) do inference_config
+        generate_inference_results(
+            truthdata, inference_config, pipeline; inference_method)
+    end
+
+    inference_results_tsk = make_inference(pipeline)
     inference_results = fetch.(inference_results_tsk)
-    @test length(inference_results) == 1
-    @test all([result["inference_results"] isa EpiAwareObservables
-               for result in inference_results])
+
+    for pipetype in [SmoothOutbreakPipeline, MeasuresOutbreakPipeline,
+        SmoothEndemicPipeline, RoughEndemicPipeline]
+        pipeline = pipetype(;ndraws = 40, testmode = true)
+        inference_results_tsk = make_inference(pipeline)
+        inference_results = fetch.(inference_results_tsk)
+        @test length(inference_results) == 1
+        @test all([result["inference_results"] isa EpiAwareObservables
+                for result in inference_results])
+    end
 end
 
 @testset "do_pipeline test: just run" begin
