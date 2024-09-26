f8d6c5be493e51ec8eace6f3f37c4d8c5d9e5ce7
Samuel Brand <sam055@mac.com>
2024-09-26 12:16:04 +0100

    split up censoring utility functions

--- EpiAware/src/EpiAwareUtils/censored_pmf.jl
+++ EpiAware/src/EpiAwareUtils/censored_pmf.jl
@@ -84,13 +84,85 @@ function _check_and_give_ts(dist::Distribution, Δd, D, upper)
     return ts
 end
 
+"""
+Calculate the CDF of the random variable `X + U` where `X` has cumulative distriubtion function `F`
+and `U` is a uniform random variable on `[0, Δd)`.
+
+This is used in solving for censored CDFs and PMFs using numerical quadrature.
+"""
+function ∫F(dist, t, Δd)
+    quadgk(u -> exp(logcdf(dist, t - u) - log(Δd)), 0.0, min(Δd, t))[1]
+end
+
+@doc raw"
+Create a discrete probability cumulative distribution function (CDF) from a given distribution,
+assuming a uniform distribution over primary event times with censoring intervals of width `Δd` for
+both primary and secondary events.
+
+NB: `censored_cdf` returns the _non-truncated_ CDF, i.e. the CDF without conditioning on the
+secondary event occuring either before or after some time.
+
+
+# Arguments
+- `dist`: The distribution from which to create the PMF.
+- `Δd`: The step size for discretizing the domain. Default is 1.0.
+- `D`: The upper bound of the domain. Must be greater than `Δd`. Default `D = nothing`
+indicates that the distribution should be truncated at its `upper`th percentile rounded
+to nearest multiple of `Δd`.
+
+
+# Returns
+- A vector representing the CDF with 0.0 appended at the beginning.
+
+# Raises
+- `AssertionError` if the minimum value of `dist` is negative.
+- `AssertionError` if `Δd` is not positive.
+- `AssertionError` if `D` is shorter than `Δd`.
+- `AssertionError` if `D` is not a multiple of `Δd`.
+
+# Examples
+
+```jldoctest filter
+using Distributions
+using EpiAware.EpiAwareUtils
+
+dist = Exponential(1.0)
+
+censored_cdf(dist; D = 10) |>
+    p -> round.(p, digits=3)
+
+# output
+10-element Vector{Float64}:
+ 0.0
+ 0.368
+ 0.768
+ 0.915
+ 0.969
+ 0.989
+ 0.996
+ 0.999
+ 1.0
+ 1.0
+ 1.0
+```
+"
+function censored_cdf(dist::Distribution; Δd = 1.0, D = nothing, upper = 0.999)
+    ts = _check_and_give_ts(dist, Δd, D, upper)
+    cens_F = ts .|> t -> ∫F(dist, t, Δd)
+    return [0.0; cens_F]
+end
+
 @doc raw"
 Create a discrete probability mass function (PMF) from a given distribution,
 assuming a uniform distribution over primary event times with censoring intervals of width `Δd` for
 both primary and secondary events. The CDF for the time from the left edge of the interval
 containing the primary event to the secondary event is created by direct numerical integration (quadrature)
-of the convolution of the CDF of `dist` with the uniform density on `[0,Δd)`, the discrete PMF
-for double censored delays is then found using simple differencing on the CDF.
+of the convolution of the CDF of `dist` with the uniform density on `[0,Δd)`, using the `censored_cdf`
+function. The discrete PMF for double censored delays is then found using simple differencing
+on the CDF.
+
+NB: `censored_pmf` returns a _right-truncated_ PMF, i.e. the PMF conditioned on the secondary event
+occurring before or on the final secondary censoring window.
 
 
 # Arguments
@@ -136,10 +208,6 @@ censored_pmf(dist; D = 10) |>
 ```
 "
 function censored_pmf(dist::Distribution; Δd = 1.0, D = nothing, upper = 0.99)
-    ts = _check_and_give_ts(dist, Δd, D, upper)
-
-    ∫F(dist, t, Δd) = quadgk(u -> exp(logcdf(dist, t - u) - log(Δd)), 0.0, Δd)[1]
-    _q = ts .|> t -> ∫F(dist, t, Δd)
-
-    return [0.0; _q] |> diff |> p -> p ./ sum(p)
+    cens_cdf = censored_cdf(dist; Δd, D, upper)
+    return cens_cdf |> diff |> p -> p ./ sum(p)
 end
