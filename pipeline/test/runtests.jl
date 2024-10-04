657ee3c096d14767061c69151beb96e1981c1585
Samuel Brand <48288458+SamuelBrand1@users.noreply.github.com>
2024-10-04 14:53:25 +0100

    Make pipeline inference tests more focused (#475)
    * fix tests and capture forecasting failures
    
    * delete stale end-to-end test

--- pipeline/test/runtests.jl
+++ pipeline/test/runtests.jl
@@ -1,10 +1,9 @@
 using DrWatson, Test
 quickactivate(@__DIR__(), "EpiAwarePipeline")
 using EpiAwarePipeline, EpiAware
-
+import Random
+Random.seed!(123)
 # Run tests
-include("pipeline/test_pipelinetypes.jl");
-include("pipeline/test_pipelinefunctions.jl");
 include("utils/test_utils.jl");
 include("constructors/test_constructors.jl");
 include("simulate/test_simulate.jl");
@@ -12,3 +11,4 @@ include("infer/test_infer.jl");
 include("forecast/test_forecast.jl");
 include("scoring/test_score_parameters.jl");
 include("plotting/plotting_tests.jl");
+include("pipeline/test_pipeline.jl");
