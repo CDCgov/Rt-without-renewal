c93cbc37b5a2eca06d5c282ab3e65b55a180314e
Samuel Brand <sam055@mac.com>
2024-10-10 15:52:20 +0100

    Explicit use of Distributions

--- EpiAware/src/EpiAwareUtils/RealValued.jl
+++ EpiAware/src/EpiAwareUtils/RealValued.jl
@@ -3,9 +3,9 @@ A type to represent real-valued distributions, the purpose of this type is to av
 with the `eltype` function when having `rand` calls in the model.
 """
 struct RealValued <: Distributions.ValueSupport end
-Base.eltype(::Type{<:Sampleable{F, RealValued}}) where {F} = Real
+Base.eltype(::Type{<:Distributions.Sampleable{F, RealValued}}) where {F} = Real
 
 """
 A constant alias for `Distribution{Univariate, RealValued}`. This type represents a univariate distribution with real-valued outcomes.
 """
-const RealUnivariateDistribution = Distribution{Univariate, RealValued}
+const RealUnivariateDistribution = Distributions.Distribution{Distributions.Univariate, RealValued}
