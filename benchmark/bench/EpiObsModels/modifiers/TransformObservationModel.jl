04e7e65f78e63831fd772f92817a1880ded07b57
Sam Abbott <azw1@cdc.gov>
2024-10-15 10:53:26 -0500

    Update TransformObservationModel.jl

--- benchmark/bench/EpiObsModels/modifiers/TransformObservationModel.jl
+++ benchmark/bench/EpiObsModels/modifiers/TransformObservationModel.jl
@@ -1,6 +1,6 @@
 let
     I_t = fill(5.0, 100)
     transform_obs = TransformObservationModel(PoissonError())
-    mdl = generate_observations(transform_obs, missing, I_t)
+    mdl = generate_observations(transform_obs, I_t, I_t)
     suite["TransformObservationModel"] = make_epiaware_suite(mdl)
 end
