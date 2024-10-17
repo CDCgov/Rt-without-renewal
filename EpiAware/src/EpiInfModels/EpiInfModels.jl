df2bed61daaf75bcb71b51f972109f947760c48a
Samuel Brand <sam055@mac.com>
2024-10-17 17:04:43 +0100

    Add InfectionODEProcess

--- EpiAware/src/EpiInfModels/EpiInfModels.jl
+++ EpiAware/src/EpiInfModels/EpiInfModels.jl
@@ -6,10 +6,14 @@ module EpiInfModels
 using ..EpiAwareBase
 using ..EpiAwareUtils
 
-using Turing, Distributions, DocStringExtensions, LinearAlgebra, LogExpFunctions
+using Turing, Distributions, DocStringExtensions, LinearAlgebra, LogExpFunctions,
+      OrdinaryDiffEq
 
 #Export models
-export EpiData, DirectInfections, ExpGrowthRate, Renewal
+export EpiData, DirectInfections, ExpGrowthRate, Renewal, InfectionODEProcess
+
+#Export parameter type
+export ODEParams
 
 #Export functions
 export R_to_r, r_to_R, expected_Rt
@@ -20,6 +24,7 @@ include("DirectInfections.jl")
 include("ExpGrowthRate.jl")
 include("RenewalSteps.jl")
 include("Renewal.jl")
+include("InfectionODEProcess.jl")
 include("utils.jl")
 
 end
