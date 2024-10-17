3d9d7ebc81def531673798195441ed38411a6bd4
Samuel Brand <sam055@mac.com>
2024-10-17 17:07:31 +0100

    add benchmark

---
+++ benchmark/bench/EpiInfModels/InfectionODEProcess.jl
@@ -0,0 +1,25 @@
+let
+    using OrdinaryDiffEq
+    r = log(2) / 7 # Growth rate corresponding to 7 day doubling time
+    u0 = [1.0]
+    p = [r]
+    params = ODEParams(u0 = u0, p = p)
+
+    # Define the ODE problem using SciML
+    # We use a simple exponential growth model
+
+    function expgrowth(du, u, p, t)
+        du[1] = p[1] * u[1]
+    end
+    prob = ODEProblem(expgrowth, u0, (0.0, 10.0), p)
+
+    # Define the InfectionODEProcess
+
+    expgrowth_model = InfectionODEProcess(prob::ODEProblem; ts = 0:1:10,
+        solver = Tsit5(),
+        sol2infs = sol -> sol[1, :])
+
+    # Generate the latent infections
+    I_t = generate_latent_infs(expgrowth_model, params)()
+    suite["InfectionODEProcess"] = make_epiaware_suite(mdl)
+end
