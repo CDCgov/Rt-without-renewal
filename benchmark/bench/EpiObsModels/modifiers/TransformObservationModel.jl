ba434b0119d2751451bf8efab21b2ff58178c889
Samuel Brand <sam055@mac.com>
2024-10-24 12:31:42 +0100

    Choose a model with a parameter to sample and document the gotcha

--- benchmark/bench/EpiObsModels/modifiers/TransformObservationModel.jl
+++ benchmark/bench/EpiObsModels/modifiers/TransformObservationModel.jl
@@ -1,6 +1,6 @@
 let
     I_t = fill(5, 100)
-    transform_obs = TransformObservationModel(PoissonError())
+    transform_obs = TransformObservationModel(NegativeBinomialError())
     mdl = generate_observations(transform_obs, I_t, I_t)
     suite["TransformObservationModel"] = make_epiaware_suite(mdl)
 end
