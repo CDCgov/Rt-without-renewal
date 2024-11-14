850ec5c9a23fa113cd2b5b48e8cc0f66e21072c9
Samuel Brand <sam055@mac.com>
2024-11-14 11:50:31 +0000

    interim commit

--- pipeline/src/constructors/make_model_priors.jl
+++ pipeline/src/constructors/make_model_priors.jl
@@ -26,3 +26,35 @@ function make_model_priors(pipeline::AbstractEpiAwarePipeline)
         "log_I0_prior" => log_I0_prior
     )
 end
+
+function make_model_priors(igp, latent_model, pipeline::AbstractEpiAwarePipeline)
+    @assert igp in [Renewal, ExpGrowthRate, DirectInfections] "`igp` must be a valid model type"
+
+    transformed_process_init_prior = Normal(0.0, 0.25)
+    std_prior = HalfNormal(0.25)
+    damp_param_prior = Beta(0.5, 0.5)
+    log_I0_prior = Normal(log(100.0), 1e-5)
+
+    return Dict(
+        "transformed_process_init_prior" => transformed_process_init_prior,
+        "std_prior" => std_prior,
+        "damp_param_prior" => damp_param_prior,
+        "log_I0_prior" => log_I0_prior
+    )
+end
+
+# function set_init_and_std_prior(epimodel)
+#     if epimodel isa Renewal
+#         init_prior = Normal(log(1.2), 0.25)
+#         std_prior = HalfNormal(0.05)
+#         return (; init_prior, std_prior)
+#     elseif epimodel isa ExpGrowthRate
+#         init_prior = Normal(0.1, 0.025)
+#         std_prior = LogNormal(log(0.025), 0.01)
+#         return (; init_prior, std_prior)
+#     elseif epimodel isa DirectInfections
+#         init_prior = Normal(log(100.0), 0.25)
+#         std_prior = HalfNormal(0.025)
+#         return (; init_prior, std_prior)
+#     end
+# end
