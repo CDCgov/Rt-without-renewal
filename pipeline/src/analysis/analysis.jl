677d222597f86ce7d12725436bc125baa82b9247
Samuel Brand <48288458+SamuelBrand1@users.noreply.github.com>
2024-12-18 18:06:29 +0000

    export new func

--- pipeline/src/analysis/analysis.jl
+++ pipeline/src/analysis/analysis.jl
@@ -1,3 +1,5 @@
+include("config_mappings.jl")
 include("make_truthdata_dataframe.jl")
 include("make_prediction_dataframe_from_output.jl")
 include("make_scoring_dataframe_from_output.jl")
+include("make_mcmc_diagnostic_dataframe.jl")
